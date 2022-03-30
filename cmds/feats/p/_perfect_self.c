82fdeea175dbb43cf1652600776d06440f2a718f
shadowgate <law@shadowgate.org>
2022-03-30 12:30:02 +0000

    Autocommit

--- cmds/feats/p/_perfect_self.c
+++ cmds/feats/p/_perfect_self.c
@@ -9,7 +9,7 @@ void create()
     feat_category("KiOffense");
     feat_name("perfect self");
     feat_prereq("Monk L20");
-    feat_desc("At 20th level, a monk becomes a magical creature and gains damage reduction both to physical and magical attacks made by a those who choose to face them.");
+    feat_desc("At 20th level, a monk becomes a magical creature and gains damage reduction both to physical and magical attacks made by their opponents.");
     permanent(1);
     set_required_for(({}));
 }
