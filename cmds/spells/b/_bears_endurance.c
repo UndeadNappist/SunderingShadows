dcc6f76ba58150dc0d2bc1473f466b451c6223fb
Swus <89095918+SwusInABox@users.noreply.github.com>
2022-04-19 18:09:44 -0400

    Remove the requirement for a target.
    A targetless casting should target oneself.

--- cmds/spells/b/_bears_endurance.c
+++ cmds/spells/b/_bears_endurance.c
@@ -17,8 +17,7 @@ void create() {
     set_description("This spell allows the caster to infuse their target with the endurance of a bear, granting them a +4 enhancement bonus to constitution.");
     set_verbal_comp();
     set_somatic_comp();
-    set_target_required(1);
-	set_helpful_spell(1);
+    set_helpful_spell(1);
 }
 
 void spell_effect(int prof) {
@@ -29,6 +28,11 @@ void spell_effect(int prof) {
         return;
     }
     if (objectp(place)) place = environment(caster);
+	
+    if(!target)
+    {
+        target = caster;
+    }
     /*
     if((int)target->query_property("augmentation")){
       tell_object(caster,"%^YELLOW%^"+target->QCN+" is already under the influence of a similar spell.");
