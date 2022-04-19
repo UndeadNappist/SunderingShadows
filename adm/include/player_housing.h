5c377541ff7b83c633e960aa14b74692d8445433
shadowgate <law@shadowgate.org>
2022-04-19 20:00:02 +0000

    Autocommit

---
+++ adm/include/player_housing.h
@@ -0,0 +1,20 @@
+#define TPWARD_LEGENDARY    75
+#define TPWARD_EPIC         65
+#define TPWARD_RARE         55
+#define TPWARD_GOOD         45
+#define TPWARD_COMMON       35
+#define TPWARD_POOR         25
+
+#define SCRYWARD_LEGENDARY  75
+#define SCRYWARD_EPIC       65
+#define SCRYWARD_RARE       55
+#define SCRYWARD_GOOD       45
+#define SCRYWARD_COMMON     35
+#define SCRYWARD_POOR       25
+
+#define LOCKS_LEGENDARY     80
+#define LOCKS_EPIC          70
+#define LOCKS_RARE          60
+#define LOCKS_GOOD          50
+#define LOCKS_COMMON        40
+#define LOCKS_POOR          30
