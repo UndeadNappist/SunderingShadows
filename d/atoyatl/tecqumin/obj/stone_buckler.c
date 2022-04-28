d65f2bc9af7e5b0005b673ab02e3f0fc3490f5cd
shadowgate <law@shadowgate.org>
2022-04-28 20:30:02 +0000

    Autocommit

--- d/atoyatl/tecqumin/obj/stone_buckler.c
+++ d/atoyatl/tecqumin/obj/stone_buckler.c
@@ -1,22 +1,21 @@
 #include <std.h>
-inherit "/d/common/obj/armour/shield";
+inherit "/d/common/obj/armour/buckler";
 
 void create() {
    ::create();
    set_name("shaped stone shield");
-   set_id(({ "shield","buckler","sone shield","stone buckler", "shaped buckler","shaped stone buckler"}));
+   set_id(({ "shield","buckler","stone shield","stone buckler", "shaped buckler","shaped stone buckler"}));
    set_short("%^CYAN%^Tecqumin %^RESET%^stone %^ORANGE%^buckler%^RESET%^");
    set_obvious_short("%^RESET%^%^ORANGE%^A %^RESET%^stone %^ORANGE%^buckler");
    set_long("This small round shield is shaped from stone. It looks like"
      +" it should be very heavy to lift, but the enchantments cast upon it make it"
-     +" light enough to wield as a buckler. Staps of leather enable it to be held in one"
+     +" light enough to wield as a buckler. Straps of leather enable it to be held in one"
      +" hand while a weapon is wielded in the other.");
    set_value(2950);
    while((int)TO->query_property("enchantment") != 6) {
      TO->remove_property("enchantment");
      TO->set_property("enchantment",6);
    }
-   set_item_bonus("shieldMiss",15);
    set_wear((:TO,"wear_it":));
    set_lore("The stone shields of the %^CYAN%^Tecqumin%^RESET%^ were carefully shaped."
     +" Mostly they were thin enough to be liftable, which significantly reduced their"
