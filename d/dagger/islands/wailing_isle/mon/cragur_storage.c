3beff7cb4b65ed67c9f84e068cac9c837b8416e5
shadowgate <law@shadowgate.org>
2022-05-15 21:00:03 +0000

    Autocommit

--- d/dagger/islands/wailing_isle/mon/cragur_storage.c
+++ d/dagger/islands/wailing_isle/mon/cragur_storage.c
@@ -4,15 +4,7 @@
 #include <std.h>
 #include <daemons.h>
 #include "../defs.h"
-inherit "/std/obj/fence_store_epic";
-
-void create(){
-    ::create();
-    set_property("no teleport",1);
-    set_properties((["light":2, "indoors":1]));
-    set("short","thief storage");
-    set("long","If you're here you shouldn't be.");
-}
+inherit "/std/obj/fence_store_high";
 
 void reset(){
     object ob, storage;
