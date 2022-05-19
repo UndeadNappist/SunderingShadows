bf471793f1601dda17e678feeb0af0f8c4b5b5da
Swus <89095918+SwusInABox@users.noreply.github.com>
2022-05-18 21:54:51 -0400

    Correction to lore and mechanical description

--- cmds/feats/s/_spellbreaker.c
+++ cmds/feats/s/_spellbreaker.c
@@ -10,7 +10,7 @@ void create()
     feat_category("MagicResistance");
     feat_name("spellbreaker");
     feat_prereq("Disruptive, Fighter L10");
-    feat_desc("You extend your wards, allowing yourself to make an attack of opportunity whenever enemy casts a spell at you.
+    feat_desc("Your instincts sharpen, allowing you to make an attack of opportunity whenever enemy casts a spell.
 
 %^BOLD%^N.B.%^RESET%^ Stacks with similar feats.");
     permanent(1);
