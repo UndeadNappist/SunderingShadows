a18996fd707b5ac5d91ca9eb056465ae77f30f1b
shadowgate <law@shadowgate.org>
2022-05-20 21:00:02 +0000

    Autocommit

--- d/dagger/monsters.h
+++ d/dagger/monsters.h
@@ -1,39 +1,74 @@
 
 #define MONSTERS (([\
+    "drow elite" : "/d/dagger/drow/mon/elite",\
+    "drow assassin" : "/d/dagger/drow/mon/drowp",\
+    "drider" : "/d/dagger/drow/temple/mon/drider",\
+    "drow warrior" : "/d/dagger/drow/mon/droww",\
+    "grimtooth" : "/d/dagger/drow/mon/shadow",\
+    "drow priest" : "/d/dagger/drow/mon/priest",\
+    "fire witch" : "/d/dagger/drow/mon/fwitch",\
+    "derro pech" : "/d/dagger/derro/mon/pech",\
+    "reaver" : "/d/dagger/marsh/swamp/mon/reaver",\
+    "krykoth" : "/d/dagger/marsh/tower/mon/krykoth",\
+    "istvan" : "/d/dagger/marsh/swamp/mon/uncle",\
+    "berserker" : "/d/dagger/cave2/mon/berserker",\
+    "ogre warrior" : "/d/dagger/cave2/mon/ogre1",\
+    "troll from Torm" : "/d/dagger/Torm/mon/troll",\
+    "skeleton warrior" : "/d/antioch/valley/mons/wskeleton",\
     "trail ghost" : "/d/antioch/valley/mons/ghost",\
     "trail ghast" : "/d/antioch/valley/mons/ghast",\
     "trail heucuva" : "/d/antioch/valley/mons/heucuva",\
     "trail haunt" : "/d/antioch/valley/mons/haunt",\
-    "pirate thief" : "/d/islands/pirates/mon/piratethief",\
-    "pirate guard" : "/d/islands/pirates/mon/pirateguard",\
     "valley feyr" : "/d/antioch/valley/mons/gfeyr",\
     "valley snake" : "/d/antioch/valley/mons/snake",\
     "valley creeper" : "/d/antioch/valley/mons/creeper",\
-    "istvan" : "/d/dagger/marsh/swamp/mon/uncle",\
-    "ogre warrior" : "/d/dagger/cave2/mon/ogre2",\
-    "maurezhi demon" : "/d/dagger/cave2/mon/maurezhi_demon.c",\
+    "pirate thief" : "/d/islands/pirates/mon/piratethief",\
+    "pirate guard" : "/d/islands/pirates/mon/pirateguard",\
+    "darkelf captain" : "/d/deku/monster/captain",\
+    "bandit captain" : "/d/deku/monster/bandit_l",\
     "native guard" : "/d/islands/tonerra/mon/nativeG",\
-    "solstice warrior" : "/d/attaya/mon/solstice",\
-    "aurumvorax" : "/d/islands/tonerra/mon/aurumvorax",\
+    "beholder":"/d/laerad/mon/beholder",\
     "amazon patrol" : "/d/islands/dallyh/forest/mon/cpatrol",\
     "amazon hunter" : "/d/islands/dallyh/forest/mon/hunter",\
-    "tonerra native guard" : "/d/islands/tonerra/mon/nativeG",\
-    "beholder":"/d/laerad/mon/beholder",\
-    "tonerra native" : "/d/islands/tonerra/mon/native",\
+    "native" : "/d/islands/tonerra/mon/native",\
     "cyclops" : "/d/islands/tonerra/mon/cyclops",\
-    "pit fiend" : "/d/attaya/tunnels/mons/pitfiend",\
-    "rock devil" : "/d/attaya/tunnels/mons/rockdevil"\
+    "aurumvorax" : "/d/islands/tonerra/mon/aurumvorax",\
+    "solstice warrior" : "/d/attaya/mon/solstice",\
+    "tonovi guard" : "/d/dagger/tonovi/mon/guard2",\
+    "tonovi captain" : "/d/dagger/tonovi/mon/guardcapt",\
+    "thorby" : "/d/dagger/road/mon/thorby",\
+    "imperial soldier" : "/d/azha/mon/s-impsoldier",\
+    "imperial guard" : "/d/azha/mon/s-impguard",\
+    "imperial scout" : "/d/azha/mon/scout",\
     "illithid" : "/d/laerad/mon/illithid",\
+    "second illithid" : "/d/laerad/mon/illithid2",\
+    "attaya undead warrior" : "/d/islands/common/mon/uwarrior.c",\
+    "argentrock fey ri" : "/d/islands/argentrock/mon/fey_ri",\
+    "argentrock elf ambusher" : "/d/islands/argentrock/mon/ambusher.c",\
+	"super solstice": "/d/attaya/mon/supersolstice",\
     "shadow demon" : "/d/islands/dreams/mons/sbeast",\
     "dreams githyanki" : "/d/islands/dreams/mons/githyanki",\
-    "attaya undead warrior" : "d/islands/common/mon/uwarrior.c",\
-    "argentrock fey ri" : "/d/islands/argentrock/mon/fey_ri",\
-    "argentrock elf ambusher" : "d/islands/argentrock/mon/ambusher.c",\
-    "muln" : "d/antioch/wild/mount/muln/mon/muln.c",\
-    "geonslu" : "d/islands/tonerra/mon/edragon.c",\
-    "cruisor tetron" : "d/islands/common/mon/cruisertetron.c",\
-    "meadowsy bloodhawk" : "/d/shadow/room/meadows/mons/bhawk.c",\
-    "meadows wererat" : "/d/shadow/room/meadows/mons/wererat.c",\
-    "feyren spider" : "/d/shadow/room/feyren/mon/monstrous_spider.c",\ 
-    "vecna" : "d/dagger/marsh/tower/mon/vecna.c",\]))
+    "pit fiend" : "/d/attaya/tunnels/mons/pitfiend",\
+    "rock devil" : "/d/attaya/tunnels/mons/rockdevil"]))
 
+/* moved these to here until/unless they're needed again
+
+    "HMQ Anti" : "/realms/garrett/move/qAntipaladin",\
+    "HMQ Fighter" : "/realms/garrett/move/qFighter",\
+    "HMQ Cleric" : "/realms/garrett/move/qCleric",\
+    "HMQ Mage" : "/realms/garrett/move/qMage",	\
+    "HMQ Thief" : "/realms/garrett/move/qThief",\
+    "HMQ Paladin" : "/realms/garrett/move/qPaladin",\
+    "HMQ Cavalier" : "/realms/garrett/move/qCavalier",\
+    "HMQ Ranger" : "/realms/garrett/move/qRanger",\
+    "HMQ Bard" : "/realms/garrett/move/qBard",\
+    "HMQ Druid" : "/realms/garrett/move/qDruid",\
+    "HMQ Assassin" : "/realms/garrett/move/qAssassin",\
+    "HMQ Fighter-Cleric-Mage" : "/realms/garrett/move/qFighter-Cleric-Mage",\
+    "HMQ Fighter-Mage-Thief" : "/realms/garrett/move/qFighter-Mage-Thief",\
+    "HMQ Fighter-Mage" : "/realms/garrett/move/qFighter-Mage",\
+    "HMQ Fighter-Cleric" : "/realms/garrett/move/qFighter-Cleric",\
+    "HMQ Fighter-Thief" : "/realms/garrett/move/qFighter-Thief",\
+    "HMQ Cleric-Mage" : "/realms/garrett/move/qCleric-Mage",\
+    "HMQ Mage-Thief" : "/realms/garrett/move/qMage-Thief",\
+*/
