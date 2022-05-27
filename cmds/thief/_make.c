973db01b36f01f2174ab328f5bff4dff569c1efb
shadowgate <law@shadowgate.org>
2022-05-27 19:30:03 +0000

    Autocommit

--- cmds/thief/_make.c
+++ cmds/thief/_make.c
@@ -1,31 +1,52 @@
 #include <std.h>
+#define SIGNS ({"friend", "enemy", "danger", "stop", "run", "mark", "idiot", "rich", "poor", "money", "kill", "mug", "lawman", "snitch"})
 
 inherit DAEMON;
 
 int cmd_make(string str){
-    object *usr;
+    object *users, player;
     int i;
 
-    if(!TP->is_class("thief") && !TP->is_class("assassin") && !TP->is_class("bard")) return 0;
-    if(str != "thief sign") return notify_fail("make what?\n");
-    if(TP->query_invis()){
-      tell_object(TP, "You sign to the room that you are a thief.");
-      return 1;
-    }
+    if(!str) return notify_fail("Make what?\n");
+    
+    player = this_player();
+    if(!player->is_class("thief") && !player->is_class("bard")) return 0;
 
-    usr = all_living(ETP);
-    for(i=0;i<sizeof(usr);i++){
-      if(!usr[i]->is_class("thief") && !usr[i]->is_class("bard") && !usr[i]->is_class("assassin")) continue;
-      if(usr[i] == TP) continue;
-      tell_object(usr[i], "You notice "+TPQCN+" signing to the room that "+TP->query_subjective()+" is a thief.");
+    users = all_living(ETP);
+    if(str == "thief sign"){
+        if(player->query_invis()){
+            tell_object(player, "%^RESET%^%^CRST%^%^C059%^You sign to the room that you are a thief.%^CRST%^");
+            return 1;
+        }
+        for(i=0;i<sizeof(users);i++){
+            if(!users[i]->is_class("thief") && !users[i]->is_class("bard")) continue;
+            if(users[i] == player) continue;
+            tell_object(users[i], "%^RESET%^%^CRST%^%^C059%^You notice "+player->query_cap_name()+"%^RESET%^%^CRST%^%^C059%^ signing to the room that "+player->query_subjective()+"%^RESET%^%^CRST%^%^C059%^ is a thief.%^CRST%^");
+        }
+        tell_object(player, "%^RESET%^%^CRST%^%^C059%^You sign to the room that you are a thief.%^CRST%^");
+        return 1;
     }
-    tell_object(TP, "You sign to the room that you are a thief.");
+    
+    if(member_array(str, SIGNS) == -1){
+            tell_object(player, "%^RESET%^%^CRST%^%^C059%^The only signs you can remember are: %^RESET%^%^CRST%^%^C124%^friend, enemy, danger, stop, run, mark, idiot, rich, poor, money, kill, mug, lawman, and snitch.%^CRST%^");
+            return 1;
+    }
+    
+    if(player->query_invis()){
+            tell_object(player, "%^RESET%^%^CRST%^%^C059%^You make a few indiscrete gestures, signing to the room: %^RESET%^%^CRST%^%^C124%^"+str+"%^RESET%^%^CRST%^%^C059%^.%^CRST%^");
+            return 1;
+    }
+    for(i=0;i<sizeof(users);i++){
+        if(!users[i]->is_class("thief") && !users[i]->is_class("bard")) continue;
+        if(users[i] == player) continue;
+        tell_object(users[i], "%^RESET%^%^CRST%^%^C059%^You notice "+player->query_cap_name()+"%^RESET%^%^CRST%^%^C059%^ make a few indiscrete gestures, signing to the room: %^RESET%^%^CRST%^%^C124%^"+str+"%^RESET%^%^CRST%^%^C059%^.%^CRST%^");
+    }
+    tell_object(player, "%^RESET%^%^CRST%^%^C059%^You make a few indiscrete gestures, signing to the room: %^RESET%^%^CRST%^%^C124%^"+str+"%^RESET%^%^CRST%^%^C059%^.%^CRST%^");
     return 1;
 }
 
 void help(){
-    write("Syntax: <make thief sign>\n\n"
-      +"Tells EVERY thief bard and assassin in the room that you are one of their brethren.  You never know who's going to be one."
-      +"If you try to make a sign to the room while hidden, you'll find it doesn't work very well."
+    write("Syntax: <make thief sign>\n        <make SIGN>\n\nTells EVERY thief and bard in the room that you are one of their brethren, or communicates a simple word. You never know who's going to be one. If you try to make a sign to the room while hidden, you'll find it doesn't work very well."
     );
 }
+
