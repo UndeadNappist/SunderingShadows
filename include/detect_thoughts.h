a90a2e348bf435ff07816ddfe260f3601cf3bfe4
Yves <mana.starved@yahoo.com>
2022-05-31 17:43:08 -0700

    Fix up detect thoughts

--- include/detect_thoughts.h
+++ include/detect_thoughts.h
@@ -3,16 +3,24 @@
     object listener;
     listeners = all_inventory(ETP);
     listeners = filter_array(listeners, (: $1->is_living() :));
-    listeners = filter_array(listeners, (:query_property("detecting_thoughts"):));
+    listeners = filter_array(listeners, (: $1->query_property("detecting_thoughts") :));
+
     if (sizeof(listeners)) {
-        foreach(listener in listeners)
-        {
-            tell_object(listener, "%^BOLD%^%^RED%^TP->QCN communicates with " + ob->QCN + ".%^RESET%^");
-            if (TP->query_stats("intelligence") - listener->query_stats("intelligence") > 10) {
-                tell_object(TP, "%^BOLD%^%^RED%^You sense your telepathy is being detected.%^RESET%^");
-                listener->set_paralyzed(2 * roll_dice(1, 4) - 1, "Interception of your detection attempts leave you stunned.");
-                listener->remove_property("detection_thoughts");
+        foreach(listener in listeners) {
+            object detect_thoughts = listener->query_property("detecting_thoughts");
+
+            if (!objectp(detect_thoughts)) continue;
+            if (listener == TP || listener == ob) continue;
+
+            if (detect_thoughts->do_save(TP)) {
+                tell_object(TP, "%^BOLD%^%^RED%^You sense your telepathic thoughts are being detected.%^RESET%^");
+                if ((TP->query_stats("intelligence") - listener->query_stats("intelligence")) > 10) {
+                    listener->set_paralyzed(roll_dice(2, 4), "Interception of your thought detection leaves you stunned!");
+                    detect_thoughts->dest_effect();
+                    continue;
+                }
             }
+            tell_object(listener, "%^BOLD%^%^RED%^" + TP->QCN + " communicates with " + ob->QCN + ".%^RESET%^");
         }
     }
 }
