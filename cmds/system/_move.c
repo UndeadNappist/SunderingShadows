4ca7ed69714828f66cb21b0904f49b19222a8174
shadowgate <law@shadowgate.org>
2022-06-03 14:30:03 +0000

    Autocommit

--- cmds/system/_move.c
+++ cmds/system/_move.c
@@ -1,72 +1,78 @@
-// File:        _move.c
-// Mudlib:      Nightmare (but see credits)
-// Purpose:     allow wizards to move any object anywhere
-// Credits:     see _refs.c
+/*
+  _move.c
+  
+  Replacement for old Nightmare III move command.
+  
+  -- Tlaloc --
+*/
 
+#include <std.h>
 #include <move.h>
 #include <daemons.h>
-#include <std.h>
-#define SYNTAX "Syntax: move <object1> to <object2>\n"
 
 inherit DAEMON;
-inherit REFS_D;
 
-
-int cmd_move( string a )
+int cmd_move(string str)
 {
-  mixed o1, o2, tmp;
-  string str;
-  int ret;
-
-    if(member_group(geteuid(previous_object()), "ambassador")) return 0;
-  if( !a || ( sscanf( a, "%s to %s", o1, o2 ) < 2 ) )
-    { notify_fail( SYNTAX ); return 0; }
-
-  seteuid( geteuid( previous_object() ) );
-
-  o1 = ref_ob( o1 );
-  if( !objectp( o1 ) )
-  {
-    notify_fail( wrap( "Could not identify " + identify( o1 ) +
-      " as an object." ) );
-    return 0;
-  }
-  o2 = ref_ob( o2 );
-  if( !objectp( o2 ) )
-  {
-    notify_fail( wrap( "Could not identify " + identify( o2 ) +
-     " as an object." ) );
-    return 0;
-  }
-  set_ref( 0, o2 );
-  write( wrap( "Moving " + identify( o1 ) + " to " + identify( o2 ) + "..." ) );
-  ret = o1-> move( o2 );
-  switch( ret )
-  {
-    case MOVE_OK:		str = "Move OK.\n"; break;
-    case MOVE_NOT_ALLOWED:	str = "Move not allowed.\n"; break;
-    case MOVE_NO_ROOM:		str = "No room in object2 for object1\n"; break;
-    case MOVE_DESTRUCTED:	str = "Move destructed\n"; break;
-    case MOVE_NO_DEST:		str = "object2 is not a destination\n"; break;
-#ifdef MOVE_TOO_HEAVY
-    case MOVE_TOO_HEAVY:	str = "object1 too heavy for object2\n"; break;
-#endif /* MOVE_TOO_HEAVY */
-#ifdef MOVE_DOOR_CLOSED
-    case MOVE_DOOR_CLOSED:	str = "The door is closed.\n"; break;
-#endif /* MOVE_DOOR_CLOSED */
-    default: str = "Error: move return code " + ret + "\n"; break;
-  }
-  if( ret == MOVE_OK ) { write( str ); return 1; }
-  notify_fail( str ); 
-  return 0;
+    object target, destination;
+    mixed t1, d1;
+    int ok;
+    
+    if(!str || !strlen(str) || sscanf(str, "%s to %s", t1, d1) < 2)
+        return notify_fail("Syntax: move <object1> to <object2>\n");
+    
+    if(t1 == "here")
+        target = environment(this_player());
+    else if(t1 == "me")
+        target = this_player();
+    else if(!(target = find_player(t1)))
+        target = find_object(t1);
+    
+    if(!target || !objectp(target))
+        return notify_fail("Target object not found.");
+    
+    if(d1 == "here")
+        destination = environment(this_player());
+    else if(d1 == "me")
+        destination = this_player();
+    else if(!(destination = find_player(d1)))
+        destination = find_object(d1);
+    
+    ok = target->move(destination);
+    
+    switch(ok)
+    {
+        case MOVE_OK:
+        write("Move OK.\n");
+        return 1;
+        break;
+        case MOVE_NOT_ALLOWED:
+        return notify_fail("Move not allowed.\n");
+        break;
+        case MOVE_NO_ROOM:
+        return notify_fail("No space in destination for target.\n");
+        break; 
+        case MOVE_DESTRUCTED:
+        return notify_fail("Move destructed.\n");
+        break;
+        case MOVE_NO_DEST:
+        return notify_fail("Destination not valid.\n");
+        break;
+        default:
+        return notify_fail("Unknown error occurred.\n");
+        break;
+    }
+    
+    return 1;
 }
 
 int help()
 {
-  write( SYNTAX +
-    "Effect: Moves object1 to inside object2\n"+
-    "See man: get_object get_objects\n"+
-    "See also: refs\n"+
-  "" );
-  return 1;
+    write("Syntax: move <target> to <destination>.\n");
+    write("Using 'here' for either will use the room you are in.\n");
+    write("Using 'me' for either will use you.\n");
+    return 1;
 }
+        
+    
+    
