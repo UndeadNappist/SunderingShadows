c7b880af1d1c00eb7c8d0c866fea883001f1b4d4
shadowgate <law@shadowgate.org>
2022-06-12 18:30:03 +0000

    Autocommit

--- d/dagger/yniam/room/cacia_shrine.c
+++ d/dagger/yniam/room/cacia_shrine.c
@@ -19,7 +19,7 @@ void create(){
     set_items(([
         "offerings" : "%^RESET%^%^CRST%^%^C109%^Placed with intention are traces of %^C245%^worldly vices%^C109%^: Bottles of spirits, cigarettes, pouches of pungent herbs. Splashes of blood, wine, and other curious substances are all %^C114%^reverently%^C109%^ left here by those seeking her favor.%^CRST%^",
         ({ "statue", "platform", "dais", "sculpture" }) : "%^RESET%^%^CRST%^%^C060%^A sinuous sculpture lurks before you, its %^C054%^alluring%^C060%^ feminine form encapsulated in solid %^C061%^darksteel%^RESET%^%^C060%^, the edge of each detail picked out in a sinister %^C053%^viol%^C052%^e%^C053%^t tarni%^C052%^s%^C053%^h%^RESET%^%^C060%^. A smile tugs at the corner of her %^C059%^swollen lips%^RESET%^%^C060%^, her comely gaze cast downward to take in your measure. Six supple arms grace her slender body, the two at her shoulders reaching high and low along her curves to seek out the %^C059%^tantalizing%^C060%^ swell of a breast and the %^C059%^shaded secrets%^C060%^ between clenched thighs. The other four slink outwards from the serpentine curve of her back, her delicate hands palm upwards, awaiting the %^C066%^offerings%^C060%^ of her supplicants.%^CRST%^",
-        "inscription" : "It reads, %^C059%^\"She Who Drinks of the World.\"%^CRST%^",
+        "inscription" : ({"%^RESET%^%^CRST%^%^C102%^Words have been carved into the curve of the stone base.%^CRST%^", "%^RESET%^%^CRST%^%^C059%^\"She Who Drinks of the World.\"%^CRST%^", "common"}),
         ]));
     set_exits(([
         "north" : ROOMS"outpost_hub",
