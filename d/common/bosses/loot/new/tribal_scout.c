88a5434fea5d73ce3f360ef9c7bf099ad8c605df
shadowgate <law@shadowgate.org>
2022-06-21 01:00:03 +0000

    Autocommit

--- d/common/bosses/loot/new/tribal_scout.c
+++ d/common/bosses/loot/new/tribal_scout.c
@@ -30,8 +30,44 @@ void create()
     set_armor_prof("medium");
     set_type("leather");
     set_property("repairtype", ({ "leatherworker" }));
+    set_limbs( ({ "torso" }) );
     set_wear((:this_object(),"wear_fun":));
     set_remove( (: this_object(),"remove_fun" :) );
 }
 
 int is_metal() { return 0; }
+
+int wear_fun()
+{   
+    if(!this_player()->is_class("druid"))
+    {
+        tell_object(this_player(), "!");
+        return 0;
+    }
+    
+    if(!strlen(owner))
+    {
+        owner = this_player()->query_name();
+        tell_object(this_player(), "");
+    }
+    else
+    {
+        if(owner != this_player()->query_name())
+        {
+            tell_object(this_player(), "This is not your armor!");
+            return 0;
+        }
+    }
+    
+    this_player()->set_property("additional_stealth_damage", ({ "force" }));
+    tell_object(this_player(), "");
+    
+    return 1;
+}
+
+int remove_fun()
+{
+    tell_object(this_player(), "");
+    this_player()->remove_property_value("additional_stealth_damage", ({ "force" }));
+    return 1;
+}
