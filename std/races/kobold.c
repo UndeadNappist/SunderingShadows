e2dbc8e298967400e051061b5e9acc33153ad31f
shadowgate <law@shadowgate.org>
2022-06-26 03:00:02 +0000

    Autocommit

--- std/races/kobold.c
+++ std/races/kobold.c
@@ -18,7 +18,7 @@ string *restricted_deities(string subrace) {
 }
 
 // stats in order: str, dex, con, int, wis, cha
-int *stat_mods(string subrace) { return ({ -2, 4, -2, 0, 0, 0 }); }
+int *stat_mods(string subrace) { return ({ -2, 4, 0, 0, 0, 0 }); }
 
 mapping skill_mods(string subrace) { return ([ "perception" : 4, "dungeoneering" : 4 ]); }
 
