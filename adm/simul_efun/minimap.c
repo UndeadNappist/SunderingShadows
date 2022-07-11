3e4cc7188715f572629875433e4b22c70e6583b2
shadowgate <law@shadowgate.org>
2022-07-11 20:00:03 +0000

    Autocommit

--- adm/simul_efun/minimap.c
+++ adm/simul_efun/minimap.c
@@ -30,7 +30,7 @@ string query_blocked(object what, string direction)
             return "#";
     }
     
-    return " ";
+    return "*";
 }
 
 varargs string simple_map(mixed arg)
@@ -57,13 +57,8 @@ varargs string simple_map(mixed arg)
         return "";
     
     extra_desc = ({  });
-    lines = allocate(7);
-    lines[0] = "---------\n";
-    lines[6] = "---------\n";
-    lines[1] = "|       |\n";
-
-    lines[2] = lines[3] = lines[4] = lines[5] = lines[1];
-    lines[3][4..4] = "%^GREEN%^*%^RESET%^";
+    lines = allocate(7, ".........\n");
+    lines[3][4..4] = "@";
     
     foreach(string dir in exits)
     {
@@ -80,30 +75,28 @@ varargs string simple_map(mixed arg)
             lines[4][4..4] = "|";
             break;
             case "east":
-            lines[3][7..7] = this_exit;
+            lines[3][6..6] = this_exit;
             lines[3][5..5] = "-";
-            lines[3][6..6] = "-";
             break;
             case "west":
-            lines[3][1..1] = this_exit;
-            lines[3][2..2] = "-";
+            lines[3][2..2] = this_exit;
             lines[3][3..3] = "-";
             break;
             case "northeast":
-            lines[1][7..7] = this_exit;
-            lines[2][6..6] = "/";
+            lines[1][6..6] = this_exit;
+            lines[2][5..5] = "/";
             break;
             case "northwest":
-            lines[1][1..1] = this_exit;
-            lines[2][2..2] = "\\";
+            lines[1][2..2] = this_exit;
+            lines[2][3..3] = "\\";
             break;
             case "southest":
-            lines[5][7..7] = this_exit;
-            lines[4][6..6] == "\\";
+            lines[5][6..6] = this_exit;
+            lines[4][5..5] == "\\";
             break;
             case "southwest":
-            lines[5][1..1] = this_exit;
-            lines[4][2..2] = "/";
+            lines[5][2..2] = this_exit;
+            lines[4][3..3] = "/";
             break;
             default:
             extra_desc += ({ dir });
@@ -112,9 +105,15 @@ varargs string simple_map(mixed arg)
     }
         
     if(sizeof(extra_desc))
-        lines[3] = replace_string(lines[3], "\n", "Also Available: " + implode(extra_desc, ", ") + "\n");
+        lines[3] = replace_string(lines[3], "\n", " Also Available: " + implode(extra_desc, ", ") + "\n");
     
     ret = lines[0] + lines[1] + lines[2] + lines[3] + lines[4] + lines[5] + lines[6];
+    ret = replace_string(ret, "@", "%^GREEN%^BOLD%^@%^RESET%^");
+    ret = replace_string(ret, "*", "%^GREEN%^*%^RESET%^");
+    ret = replace_string(ret, "-", "%^CYAN%^-%^RESET%^");
+    ret = replace_string(ret, "\\", "%^CYAN%^\\%^RESET%^");
+    ret = replace_string(ret, "/", "%^CYAN%^/%^RESET%^");
+    ret = replace_string(ret, "|", "%^CYAN%^|%^RESET%^");
     return ret;
 }
         
