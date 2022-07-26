05cbb782f50d38c53dc399348771dacbac3e5c1d
shadowgate <law@shadowgate.org>
2022-07-26 16:30:02 +0000

    Autocommit

--- d/islands/dallyh/forest/mon/pixie.c
+++ d/islands/dallyh/forest/mon/pixie.c
@@ -83,6 +83,7 @@ void create()
 //               "spellcraft" for magical creatures (elementals, sprites, will o wisp, etc...)
 //               "dungeoneering" for constructs (clockwork creatures, golems, etc...)
 int is_tamable(){ return 1; }
+int is_sentient(){ return 1; }
 int query_tame_DC(){ return 40; }
 string query_DC_type(){ return "spellcraft"; }
 
