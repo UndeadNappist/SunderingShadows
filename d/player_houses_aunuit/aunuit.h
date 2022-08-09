642e23f73c844ce1bb5ca53273072b6e5ace88a9
shadowgate <law@shadowgate.org>
2022-08-09 12:30:02 +0000

    Autocommit

--- d/player_houses_aunuit/aunuit.h
+++
@@ -1,4 +0,0 @@
-#define ROOMS "/d/player_houses/aunuit/"+
-#define SILOBJ "/d/player_houses/silesse/obj/"+
-#define OBJ "/d/player_houses/aunuit/obj/"+
-#define AUNUIT "/d/player_houses/aunuit/storage/room_inherit.c"
