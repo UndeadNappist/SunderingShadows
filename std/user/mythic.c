9790c5c4a16f14c3d034f4c711eb9698f846f935
shadowgate <law@shadowgate.org>
2022-08-12 20:30:01 +0000

    Autocommit

---
+++ std/user/mythic.c
@@ -0,0 +1,40 @@
+/*
+  mythic.c
+  
+  Handles the Awakening Soul (Mythic paths) system.
+  
+  -- Tlaloc --
+*/
+
+mapping mythic = ([ 
+                    "soul power" : 0,
+                    "path"  : 0,
+                    "level" : 0,
+                 ]);
+                 
+
+int is_awakened()
+{
+    if(this_object()->query_level() < 50)
+        return 0;
+    
+    if(!this_object()->query("awakened quest"))
+        return 0;
+    
+    return 1;
+}
+
+mixed set_mythic(string str, mixed arg)
+{
+    string *map_keys;
+    if(!stren(str) || !arg)
+        return 0;
+    
+    map_keys = keys(mythic);
+    
+    if(member_array(str, map_keys) < 0)
+        return 0;
+    
+    return mythic[str] = arg;
+}   
+    
