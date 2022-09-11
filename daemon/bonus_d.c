9657ca0dae7fb62d4ddbc64f7357dd042d98c780
shadowgate <law@shadowgate.org>
2022-09-11 15:00:02 +0000

    Autocommit

--- daemon/bonus_d.c
+++ daemon/bonus_d.c
@@ -712,7 +712,7 @@ int intimidate_check(object victim, object attacker, int mod)
     if(result == 20)
         return 1;
     
-    if(attacker->query_race() == "half-orc" || attacker->query_race() == "orc" || attacker->query_race() == "hobgoblin")
+    if(attacker->query_race() == "half-orc" || attacker->query_race() == "orc" || attacker->query_race() == "hobgoblin" || attacker->query_race() == "dragonkin")
         mod += 2;
     
     result = influence + result;
