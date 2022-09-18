3e31f049541c12cc425269c6c7dedb4395690b8b
shadowgate <law@shadowgate.org>
2022-09-18 04:30:02 +0000

    Autocommit

--- cmds/feats/h/_hideous_blow.c
+++ cmds/feats/h/_hideous_blow.c
@@ -14,6 +14,8 @@
 
 inherit FEAT;
 
+object *weapons;
+
 void create()
 {
     ::create();
@@ -76,6 +78,12 @@ void execute_feat()
         dest_effect();
         return;
     }
+    if(!sizeof(weapons = caster->query_wielded()))
+    {
+        tell_object(caster, "You need a weapon to use hideous blow.");
+        dest_effect();
+        return;
+    }
     
     ::execute_feat();
     
@@ -103,3 +111,65 @@ void execute_feat()
     tell_object(target, "%^C244%^" + caster->query_cap_name() + " infuses " + caster->query_possessive() + " weapon with chaotic e%^C145%^l%^C151%^d%^C157%^r%^C244%^itch e%^C145%^n%^C151%^e%^C157%^r%^C244%^gy and levies a h%^C145%^i%^C151%^d%^C157%^e%^C244%^ous b%^C145%^l%^C151%^o%^C244%^w at you!%^CRST%^");
     tell_object(caster, "%^C244%^" + caster->query_cap_name() + " infuses " + caster->query_possessive() + " weapon with chaotic e%^C145%^l%^C151%^d%^C157%^r%^C244%^itch e%^C145%^n%^C151%^e%^C157%^r%^C244%^gy and levies a h%^C145%^i%^C151%^d%^C157%^e%^C244%^ous b%^C145%^l%^C151%^o%^C244%^w at %^C151%^" + target->query_cap_name() + ".%^CRST%^", ({ caster, target }));
 }
+
+void execute_attack()
+{
+    int glvl, mod, roll, dmg;
+    string my_name, your_name;
+    
+    if(!caster || caster->query_unconscious())
+    {
+        dest_effect();
+        return;
+    }
+    
+    ::execute_attack();
+    caster->remove_property("using instant feat");
+    
+    if(!target)
+        target = caster->query_current_attacker();
+    
+    if(!objectp(target) || !present(target, place))
+    {
+        tell_object(caster, "Your target is no longer here.");
+        dest_effect();
+        return;
+    }
+    
+    if(!pointerp(weapons) || !objectp(weapons[0]))
+    {
+        tell_object(caster, "You have no weapon!");
+        dest_effect();
+        return;
+    }
+    
+    roll = (int)BONUS_D->process_hit(caster, target, 1, 0, 0, 0);
+    
+    if(!roll)
+    {
+        tell_object(caster, "%^WHITE%^" + target->QCN + " jumps aside and avoids your hideous blow!%^RESET%^");
+        tell_object(target, "%^WHITE%^You jump aside and avoid the hideous blow!%^RESET%^");
+        tell_room(place, target->QCN + " jumps aside and avoids the hideous blow!", ({ caster, target }));
+        return;
+    }
+    
+    glvl = caster->query_guild_level("warlock");
+    mod = BONUS_D->query_stat_bonus(caster, "charisma") * FEATS_D->has_feat(caster, "agonizing blast");
+    wepname = weapons[0]->query_name();
+    my_name = caster->query_cap_name();
+    your_name = target->query_cap_name();
+    
+    target->cause_typed_damage(target, target->return_target_limb(), roll_dice(glvl, 6), weapons[0]->query_damage_type());
+    
+    if(FEATS_D->has_feat(caster, "lifedrinker blast") && objectp(target))
+    {
+        tell_object(attacker,"%^RESET%^%^MAGENTA%^You rake your " + wepname + "%^CRST%^%^RESET%^%^MAGENTA%^ across " + your_name + "%^CRST%^%^RESET%^%^MAGENTA%^, leeching a t%^GREEN%^as%^MAGENTA%^t%^GREEN%^e %^MAGENTA%^of " + your_poss + " energy!%^RESET%^");
+        tell_object(target,"%^RESET%^%^MAGENTA%^" + my_name + "%^CRST%^%^RESET%^%^MAGENTA%^ rakes " + my_poss + " " + wepname + "%^CRST%^%^RESET%^%^MAGENTA%^ across you, and you feel slightly w%^GREEN%^ea%^MAGENTA%^ke%^GREEN%^ne%^MAGENTA%^d!%^RESET%^");
+        tell_room(here,"%^RESET%^%^MAGENTA%^" + my_name + "%^CRST%^%^RESET%^%^MAGENTA%^ rakes " + my_poss + " " + wepname + "%^CRST%^%^RESET%^%^MAGENTA%^ across " + your_name + "%^CRST%^%^RESET%^%^MAGENTA%^, and " + target->query_subjective() + " looks slightly w%^GREEN%^ea%^MAGENTA%^ke%^GREEN%^ne%^MAGENTA%^d!%^RESET%^",({ attacker, target }));
+        target->cause_typed_damage(target, target->return_target_limb(), 5 + glvl, "negative energy");
+        target->add_hp(glvl);
+    }
+}
+    
+    
+    
