4cba1a9e7beef2df74e8d26a9bb717aca0b09f57
shadowgate <law@shadowgate.org>
2022-09-22 23:30:02 +0000

    Autocommit

--- cmds/spells/p/_pull_of_the_grave.c
+++ cmds/spells/p/_pull_of_the_grave.c
@@ -10,6 +10,7 @@ void create()
     set_spell_name("pull of the grave");
     set_spell_level(([ "warlock" : 4 ]));
     set_spell_sphere("necromancy");
+    set_heritage("gloom");
     set_syntax("cast CLASS pull of the grave on TARGET");
     set_damage_desc("Death or negative energy and shaken");
     set_description("You condemn your target to the cold, unrelenting oblivion of the grave. Shards of necrotic steel swirl around them, forming a dark and forbidding coffing from whence few will ever return. On a failed combat death save, the target will obey the call and die instantly. On a successful save, the target is shaken by their close brush with death and take normalized negative energy damage as they wallow in their blackened steel tomb.");
