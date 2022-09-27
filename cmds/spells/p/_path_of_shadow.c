9eb647158dc7ae719a7235d1c25ad505aefbe0bf
shadowgate <law@shadowgate.org>
2022-09-27 20:00:02 +0000

    Autocommit

--- cmds/spells/p/_path_of_shadow.c
+++ cmds/spells/p/_path_of_shadow.c
@@ -14,8 +14,8 @@ void create()
     set_spell_sphere("alteration");
     set_syntax("cast CLASS path of shadow");
     set_damage_desc("Fast Healing 2 on caster");
-    set_description("The Shadows, bringer of death, but also the embrace of a dark mother. Feed upon the shadows and find yourself nourished.
-
+    set_description("The Shadows, bringer of death, but also the embrace of a dark mother. Feed upon the shadows and find yourself nourished.\
+\
 %^BOLD%^%^RED%^See also:%^RESET%^ status effects");
 	set_helpful_spell(1);
     set_feats_required(([ "warlock" : "tome of ancient secrets"]));
@@ -23,7 +23,7 @@ void create()
 
 string query_casting_string()
 {
-    return "%^BOLD%^%^BLACK%^" + caster->QCN + " puts their hands together where e%^RESET%^%^GREEN%^l%^BOLD%^d%^RESET%^r%^BOLD%^i%^BLACK%^tch e%^RESET%^%^GREEN%^n%^BOLD%^e%^RESET%^r%^BOLD%^g%^BLACK%^y f%^RESET%^%^GREEN%^o%^BOLD%^r%^RESET%^m%^BOLD%^s%^BLACK%^.";
+    return "%^RESET%^%^CRST%^%^C110%^"+caster->query_cap_name()+"%^RESET%^%^CRST%^%^C110%^ places their hands together, gathering %^C059%^e%^C243%^ld%^C249%^r%^C059%^i%^C243%^tc%^C249%^h %^C059%^e%^C243%^ne%^C249%^r%^C059%^g%^C243%^y %^RESET%^%^C110%^.%^CRST%^";
 }
 
 int preSpell()
@@ -32,7 +32,7 @@ int preSpell()
         target = caster;
     if(target->query_property("fast_healing_spell"))
     {
-        tell_object(caster,"%^BOLD%^%^BLACK%^You feel your spell repelled...");
+        tell_object(caster,"%^RESET%^%^CRST%^%^C059%^You feel your spell repelled...%^CRST%^");
         return 0;
     }
     return 1;
@@ -47,10 +47,10 @@ void spell_effect()
         return;
     }
 
-    tell_room(place,"%^BOLD%^%^BLACK%^The surrounding shadows feed "+caster->QCN+"'s body.");
+    tell_room(place, "%^RESET%^%^CRST%^%^C110%^The surrounding %^C059%^s%^C243%^ha%^C249%^d%^C059%^o%^C243%^ws %^RESET%^%^C110%^feed "+caster->query_cap_name()+"%^RESET%^%^CRST%^%^C110%^'s body.%^CRST%^", caster);
 
     duration = clevel * ROUND_LENGTH * 10;
-    tell_object(caster,"%^BOLD%^%^BLACK%^You feel emboldened by the shadows around you.");
+    tell_object(caster, "%^RESET%^%^CRST%^%^C110%^You feel emboldened by the %^C059%^s%^C243%^ha%^C249%^d%^C059%^o%^C243%^ws %^RESET%^%^C110%^around you.%^CRST%^");
     caster->set_property("spelled",({TO}));
     caster->set_property("fast healing",2);
     caster->set_property("fast_healing_spell",1);
@@ -63,14 +63,15 @@ void spell_effect()
 
 void dest_effect()
 {
-    if(objectp(target))
+    if(objectp(caster))
     {
-        tell_object(target,"%^BOLD%^%^BLACK%^The elation you felt begins to ebb as the shadows around you dissappate.%^RESET%^");
-        target->remove_property_value("spelled", ({TO}) );
-        target->set_property("fast healing",-2);
-        target->remove_property("fast_healing_spell");
+        tell_object(caster, "%^RESET%^%^CRST%^%^C110%^The elation you felt begins to ebb as the %^C059%^s%^C243%^ha%^C249%^d%^C059%^o%^C243%^ws %^RESET%^%^C110%^around you %^C243%^d%^C244%^i%^C245%^s%^C246%^s%^C247%^i%^C248%^p%^C249%^a%^C250%^t%^C251%^e%^RESET%^%^C110%^.%^CRST%^");
+        caster->remove_property_value("spelled", ({TO}) );
+        caster->set_property("fast healing",-2);
+        caster->remove_property("fast_healing_spell");
     }
     ::dest_effect();
     if(objectp(TO))
         TO->remove();
 }
+
