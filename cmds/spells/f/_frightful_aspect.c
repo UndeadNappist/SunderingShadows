09b1e7c66575497a5ab23b8d8d6abeb84435d862
shadowgate <law@shadowgate.org>
2022-10-03 12:30:02 +0000

    Autocommit

--- cmds/spells/f/_frightful_aspect.c
+++ cmds/spells/f/_frightful_aspect.c
@@ -20,7 +20,6 @@ void create()
     set_description("You become a larger, awful version of yourself. You grow in size, and take on features that horrify your enemies. You gain the following abilities: a +6 size bonus to Strength, a +4 size bonus to Constitution, a +6 natural armor bonus, 5 damage resistance, +2 to save against spells, and spell damage resistance equal to 10 + half your caster level. You also emit an aura that emanates 30 feet from you. Enemy creatures within the aura are shaken. This spell won't work together with iron body, stoneskin, or other such spells.\n\n%^BOLD%^%^RED%^See also:%^RESET%^ status effects");
     set_verbal_comp();
     set_somatic_comp();
-    splash_spell(2);
     set_helpful_spell(1);
 }
 
