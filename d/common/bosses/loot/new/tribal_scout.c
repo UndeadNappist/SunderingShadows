12989dbc626f6d5e4b92015768171f0baf43e349
shadowgate <law@shadowgate.org>
2022-10-06 22:30:02 +0000

    Autocommit

--- d/common/bosses/loot/new/tribal_scout.c
+++ d/common/bosses/loot/new/tribal_scout.c
@@ -16,10 +16,10 @@ void create()
 {
     ::create();
     set_name("tribal scout");
-    set_short("");
-    set_obvious_short("");
-    set_long("");
-    set_lore("");
+    set_short("IDENTIFIED SHORT");
+    set_obvious_short("NOT IDENTIFIED SHORT");
+    set_long("LONG DESC");
+    set_lore("LORE DESC - ADD IN WHAT IT DOES");
     set_property("lore difficulty", 40);
     set_property("id difficulty", 40);   
     set_flag_required("boss avatar");
@@ -33,6 +33,7 @@ void create()
     set_limbs( ({ "torso" }) );
     set_wear((:this_object(),"wear_fun":));
     set_remove( (: this_object(),"remove_fun" :) );
+    set_struck( (: this_object(), "strike_fun" :) );
     
     set_property("lore difficulty", 40);
     set_property("id difficulty", 40);
@@ -56,7 +57,7 @@ int wear_fun()
     if(!strlen(owner))
     {
         owner = this_player()->query_name();
-        tell_object(this_player(), "");
+        tell_object(this_player(), "FIRST TIME WEARING MESSAGE");
     }
     else
     {
@@ -68,14 +69,35 @@ int wear_fun()
     }
     
     this_player()->set_property("additional stealth damage", ({ "force" }));
-    tell_object(this_player(), "");
+    tell_object(this_player(), "MESSAGE WHEN PUTTING THIS ON");
     
     return 1;
 }
 
 int remove_fun()
 {
-    tell_object(this_player(), "");
+    tell_object(this_player(), "MESSAGE WHEN TAKING THIS OFF");
     this_player()->remove_property_value("additional stealth damage", ({ "force" }));
     return 1;
 }
+
+int strike_fun(int x, object wep, object attacker)
+{
+    int damage;
+    object wearer, room;
+    
+    if(!random(3))
+    {
+        wearer = environment(this_object());
+        wearer && room = environment(wearer);
+        
+        tell_object(attacker, "ATTACKER STRUCK MESSAGE");
+        tell_object(wearer, "WEARER STRUCK MESSAGE");
+        tell_room(room, "ROOM STRUCK MESSAGE", ({ source, wearer }));
+        
+        damage = roll_dice(1 + wearer->query_base_character_level() / 5, 6);
+        attacker->cause_typed_damage(attacker, attacker->return_target_limb(), damage, "piercing");
+    }
+    
+    return x;
+}       
