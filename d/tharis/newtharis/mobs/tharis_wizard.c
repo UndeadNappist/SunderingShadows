3a592eb720db2e825df99327c8111d859dc5ee71
shadowgate <law@shadowgate.org>
2022-10-19 01:30:02 +0000

    Autocommit

--- d/tharis/newtharis/mobs/tharis_wizard.c
+++ d/tharis/newtharis/mobs/tharis_wizard.c
@@ -7,7 +7,7 @@ inherit "/std/identifier";
 void create() {
     ::create();
     set_gender("male");
-    set_name("Azakoth Mor'Geran, Tharis city wizard");
+    set_name("Azakoth Mor'Geran, the city wizard");
     set_race("ogre-mage");
     set_alignment(5);
     set_class("mage");
@@ -28,6 +28,6 @@ void create() {
     set_detect_uses(4);
     set_highmod(4);
     set_id(({ "azakoth", "wizard"}));
-    set_short("%^RESET%^%^C063%^Azakoth Mor'Geran%^RESET%^%^C061%^, %^RESET%^%^C241%^Tharis %^RESET%^%^C061%^city %^RESET%^%^C024%^w%^C031%^i%^C038%^z%^C045%^a%^C031%^r%^C024%^d%^CRST%^");
+    set_short("%^RESET%^%^C063%^Azakoth Mor'Geran%^RESET%^%^C061%^, the city %^RESET%^%^C024%^w%^C031%^i%^C038%^z%^C045%^a%^C031%^r%^C024%^d%^CRST%^");
     set_long("%^RESET%^%^C061%^This massive %^RESET%^%^C063%^ogre-mage %^RESET%^%^C061%^towers over most other inhabitants of the bazaar, even while sitting at his oversized booth %^RESET%^%^C061%^surrounded by shelves of %^RESET%^%^C144%^scrolls %^RESET%^%^C061%^and %^RESET%^%^C094%^wands%^RESET%^%^C061%^. Tall %^RESET%^%^C241%^b%^C242%^l%^C243%^a%^C242%^c%^C241%^k h%^RESET%^%^C242%^o%^C243%^r%^C242%^n%^C241%^s %^RESET%^%^C061%^curl from his forehead and thick %^RESET%^%^C240%^raven-black hair %^RESET%^%^C061%^cascades down his back bound with bands of %^RESET%^%^C220%^g%^C226%^o%^RESET%^%^C227%^l%^RESET%^%^C220%^d %^RESET%^%^C061%^and %^RESET%^%^C251%^s%^C252%^i%^C253%^l%^C254%^v%^C252%^e%^C251%^r%^RESET%^%^C061%^. His %^RESET%^%^C087%^a%^C123%^z%^C159%^u%^C123%^r%^RESET%^%^C087%^e e%^RESET%^%^C123%^y%^C159%^e%^RESET%^%^C087%^s %^RESET%^%^C051%^burn %^RESET%^%^C061%^with an %^RESET%^%^C033%^a%^C039%^r%^C045%^c%^C051%^a%^C039%^n%^C033%^e l%^RESET%^%^C045%^i%^C051%^g%^C045%^h%^C033%^t %^RESET%^%^C061%^and his relatively fine-featured face is interrupted by long %^RESET%^%^C230%^tusks %^RESET%^%^C061%^that jut from his lower jaw. Rich robes of %^RESET%^%^C220%^g%^C226%^o%^C227%^l%^C220%^d %^RESET%^%^C061%^and %^RESET%^%^C241%^black %^RESET%^%^C061%^do little to hide the shape of his broad %^RESET%^%^C062%^muscular body %^RESET%^%^C061%^as he flips through a tome far too small for him, %^RESET%^%^C087%^peering %^RESET%^%^C061%^at it through a %^RESET%^%^C220%^m%^RESET%^%^C226%^o%^C227%^n%^C228%^o%^C227%^c%^C226%^l%^RESET%^%^C220%^e %^RESET%^%^C061%^as he waits for his next customer.%^CRST%^"); 
 }
