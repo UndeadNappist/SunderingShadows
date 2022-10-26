2681ef2c05e62608c5d9ab7101f89c5c9edc00fe
shadowgate <law@shadowgate.org>
2022-10-26 12:30:03 +0000

    Autocommit

--- cmds/feats/p/_parry.c
+++ cmds/feats/p/_parry.c
@@ -10,7 +10,7 @@ void create() {
     feat_type("permanent");
     feat_category("WeaponMastery");
     feat_name("parry");
-    feat_desc("The Parry feat will give you a chance based on your athletics skill to parry all attacks that are directed your way. This feat is only available to fighters and cavaliers as a class skill, and can substitute for blade block and unassailable parry. Note you must be wielding a melee weapon in your main hand for this feat to work.
+    feat_desc("The Parry feat will give you a chance based on your athletics skill to parry all attacks that are directed your way. This feat is only available to fighters and paladins as a class skill, and can substitute for blade block and unassailable parry. Note you must be wielding a melee weapon in your main hand for this feat to work.
 
 %^BOLD%^N.B.%^RESET%^ Parry, unassailable parry, unarmed parry, and blade block feats do not stack. Only one will apply at a time.
 %^BOLD%^N.B.%^RESET%^ Parry does not work with ranged weapons.");
@@ -24,9 +24,7 @@ int allow_shifted() { return 1; }
 int prerequisites(object ob) {
     if(!objectp(ob)) { return 0; }
     if(!ob->is_class("fighter") &&
-        !ob->is_class("cavalier") &&
         !ob->is_class("paladin") &&
-        !ob->is_class("antipaladin") &&
         !FEATS_D->has_feat(ob, "fighter training")) {
         dest_effect();
         return 0;
