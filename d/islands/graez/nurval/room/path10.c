5c93f7af6a5dd837448f6d2335d9084deaf6f2e7
shadowgate <law@shadowgate.org>
2022-11-09 04:30:02 +0000

    Autocommit

--- d/islands/graez/nurval/room/path10.c
+++ d/islands/graez/nurval/room/path10.c
@@ -2,21 +2,22 @@ inherit ROOM;
 
 void create(){
     ::create();
-    set_name("Path through the mountains");
-    set_short("%^RESET%^%^C059%^Path through the mountains%^CRST%^");
-    set_long("%^CRST%^Sheer %^RESET%^%^C248%^r%^C245%^o%^C242%^c%^RESET%^%^C248%^k w%^RESET%^%^C246%^a%^C242%^l%^C243%^l%^RESET%^%^C248%^s%^CRST%^ rise up along either side of the %^RESET%^%^C058%^path %^CRST%^here, which continues to climb toward the entrance of a %^RESET%^%^C241%^cave%^CRST%^. To the south the trail continues along the inside of the towering %^RESET%^%^C240%^walls%^CRST%^.%^CRST%^");
+    set_name("Before the cave");
+    set_short("%^RESET%^%^C059%^Before the cave%^CRST%^");
+    set_long("%^CRST%^You stand before the shadow-shrouded entrance of a cave. The air here is noticeably cold and the air practically hums with an unsettling energy. Likely the wisest decision would be to make one's down the stairs and as far away from this place as possible.%^CRST%^");
     set_items(([
         "sky" : "%^RESET%^%^CRST%^The sky above is %^RESET%^%^C242%^c%^C246%^l%^C248%^o%^C241%^t%^C243%^t%^C245%^e%^C242%^d %^CRST%^with preternatural %^RESET%^%^C240%^darkness%^CRST%^.%^CRST%^",
         "walls": "%^RESET%^%^C244%^Even from this vantage point, the %^RESET%^%^C240%^coal-black walls %^RESET%^%^C244%^enclosing the %^RESET%^%^C246%^z%^C248%^i%^C246%^g%^C244%^g%^RESET%^%^C246%^u%^RESET%^%^C248%^r%^RESET%^%^C250%^a%^RESET%^%^C246%^t%^RESET%^%^C244%^ are towering.%^CRST%^",
         "ziggurat" : "%^RESET%^%^C244%^This massive, tiered structure scrapes the %^RESET%^%^C240%^d%^C241%^a%^C242%^r%^C243%^k%^C242%^e%^C241%^n%^C240%^e%^C241%^d %^RESET%^%^C240%^s%^C241%^k%^C242%^y%^C244%^, made of %^RESET%^%^C240%^coal-black stones %^C244%^similar to the walls.%^CRST%^\n",
    ]));
    
-    set_smell("default","%^RESET%^%^C241%^A slightly metallic scent tinges the air.%^CRST%^");
-    set_listen("default","%^RESET%^%^C244%^There is an unsettling quiet here.%^CRST%^");
+    set_smell("default","%^RESET%^%^C241%^The chill air here is slightly tinged with a metallic scent.%^CRST%^");
+    set_listen("default","%^RESET%^%^C244%^The air practically hums with the vibration of magical energy.%^CRST%^");
 
     set_exits(([
-                "north": "/d/islands/graez/nurval/room/path9",
-                "south": "/d/islands/graez/nurval/room/path7",
+                "cave": "/d/islands/graez/nurval/room/cave",
+                "stairs": "/d/islands/graez/nurval/room/path9",
+                
                    ]));
   
 }
