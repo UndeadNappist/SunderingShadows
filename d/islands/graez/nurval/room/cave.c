7db6893711bde066afd71d1788f81d01c39e4fc0
shadowgate <law@shadowgate.org>
2022-11-09 06:00:02 +0000

    Autocommit

--- d/islands/graez/nurval/room/cave.c
+++ d/islands/graez/nurval/room/cave.c
@@ -8,7 +8,7 @@ void create(){
     set_terrain(BUILT_TUNNEL);
     set_light(1);
     set_indoors(1);
-    set_long("%^RESET%^%^CRST%^");
+    set_long("%^RESET%^%^RESET%^%^C059%^This massive cave is awash in deep, shifting %^RESET%^%^C240%^s%^C241%^h%^C242%^a%^C243%^d%^C242%^o%^C241%^w%^RESET%^%^C240%^s %^RESET%^%^C059%^that set the %^RESET%^%^C088%^n%^C124%^e%^C160%^r%^C196%^v%^C124%^e%^RESET%^%^C088%^s %^RESET%^%^C059%^on edge and let the imagination run wild with what manner of vile creatures might be hidden in their %^RESET%^%^C240%^depths%^RESET%^%^C059%^.%^CRST%^");
     set_items(([
         
    ]));
