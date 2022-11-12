11ff95d35b840b320be36683afd6b2aa0f5ff6e6
shadowgate <law@shadowgate.org>
2022-11-12 01:30:04 +0000

    Autocommit

--- d/islands/graez/rooms/swamp/18.c
+++ d/islands/graez/rooms/swamp/18.c
@@ -11,7 +11,7 @@ void create(){
     set_long(::query_long()+"\n%^CRST%^Toward the east, the ground begins to ascend, a path leading up out of the swamp and through the %^RESET%^%^C058%^foothills %^CRST%^where the massive %^RESET%^%^C246%^z%^C248%^i%^C246%^g%^C244%^g%^RESET%^%^C246%^u%^RESET%^%^C248%^r%^RESET%^%^C250%^a%^RESET%^%^C246%^t %^RESET%^of %^RESET%^%^C241%^Nurval%^CRST%^ can be seen, the skies preternaturally %^RESET%^%^C240%^dark %^RESET%^within its proximity.%^CRST%^\n");
     set_exits(([
         "northwest" : SWAM"17",
-        "east" : "/d/dagger/nurval/room/tomb1",
+        "east" : "/d/islands/graez/nurval/room/path1",
         ]));
 }
 
