de0caeb8576cb071568625af11ff57e8aab49d65
shadowgate <law@shadowgate.org>
2022-12-02 18:30:02 +0000

    Autocommit

--- d/magic/obj/firewall.c
+++ d/magic/obj/firewall.c
@@ -13,8 +13,8 @@ void remove_wall();
 void create() {
    ::create();
    set_name("wall of fire");
-   set_short("%^BOLD%^%^RED%^A %^RESET%^%^MAGENTA%^w%^BOLD%^%^RED%^all %^RESET%^%^MAGENTA%^o%^BOLD%^%^RED%^f %^RESET%^%^MAGENTA%^f%^BOLD%^%^RED%^ire%^RESET%^");
-   set_long("%^BOLD%^%^RED%^You see a massive opaque sheet of fire raging wildly upward in shades of %^MAGENTA%^violet %^RED%^and red.");
+   set_short("%^RESET%^%^CRST%^%^C124%^A wall of %^C091%^f%^C097%^i%^C091%^re%^CRST%^");
+   set_long("%^RESET%^%^CRST%^%^C124%^You see a massive opaque sheet of fire raging wildly upward in shades of %^C091%^vi%^C097%^o%^C091%^le%^C097%^t %^RESET%^%^C124%^%^and %^C196%^red%^C124%^.%^CRST%^");
    set_id( ({"wall","wall of fire","firewall","fire","mirror"}) );
    set_weight(10000);
    set_value(10000);
@@ -41,8 +41,8 @@ void surround(object ob, object spell) {
    spellob = spell;
    wallname = whose+"surroundingfirewall";
    if(present(wallname,environment(caster))) {
-      tell_object(caster,"The new wall simply melts into the wall that is already surrounding you!\n");
-      tell_room(environment(caster),"The wall surrounding "+caster->query_cap_name()+" glows brighter for a second.\n");
+      tell_object(caster,"%^RESET%^%^CRST%^%^C124%^The new wall simply melts into the wall that is already surrounding you!\n%^CRST%^");
+      tell_room(environment(caster),"%^RESET%^%^CRST%^%^C124%^The wall surrounding "+caster->query_cap_name()+"%^RESET%^%^CRST%^%^C124%^ glows brighter for a second.\n%^CRST%^");
       if(objectp(query_property("spell")))
          query_property("spell")->dest_effect();
       return;
@@ -51,7 +51,7 @@ void surround(object ob, object spell) {
    j = sizeof(foes);
 
    if(j)
-       tell_room(environment(foes[k]),"%^BOLD%^%^RED%^"+caster->QCN+"'s enemies are singed by the flames and leaps back as they rise up!",foes[k]);
+       tell_room(environment(foes[k]),"%^RESET%^%^CRST%^%^C124%^"+caster->QCN+"%^RESET%^%^CRST%^%^C124%^'s enemies are singed by the %^C091%^fl%^C097%^a%^C091%^me%^C097%^s%^RESET%^%^C124%^ and leap back as they rise up!%^CRST%^",foes[k]);
    for(k=0;k<j;k++)
    {
        if(!present(wallname,environment(caster))) continue;
@@ -59,7 +59,7 @@ void surround(object ob, object spell) {
           strsrch(foes[k]->query_property("strength"),"fire") != -1)
            continue;
        dmg = spellob->query_base_damage();
-       tell_object(foes[k],"%^BOLD%^%^RED%^You get burned by the flames!");
+       tell_object(foes[k],"%^RESET%^%^CRST%^%^C124%^You get burned by the %^C091%^fl%^C097%^a%^C091%^me%^C097%^s%^RESET%^%^C124%^!%^CRST%^");
        foes[k]->cause_typed_damage(foes[k],foes[k]->return_target_limb(),dmg,"fire" );
 
        if(objectp(foes[k]))
@@ -89,7 +89,7 @@ void monitor() {
        foes = caster->query_attackers();
        j = sizeof(foes);
        if (j) {
-           tell_room(environment(TO), "%^BOLD%^%^RED%^" + caster->QCN + "'s enemies are singed by the flames!", foes[k]);
+           tell_room(environment(TO), "%^RESET%^%^CRST%^%^C124%^"+caster->QCN+"%^RESET%^%^CRST%^%^C124%^'s enemies are singed by the %^C091%^fl%^C097%^a%^C091%^me%^C097%^s%^RESET%^%^C124%^!%^CRST%^", foes[k]);
            for (k = 0; k < j; k++) {
                if (!objectp(foes[k])) {
                    continue;
@@ -98,7 +98,7 @@ void monitor() {
                    strsrch(foes[k]->query_property("strength"), "fire") != -1) {
                    continue;
                }
-               tell_object(foes[k], "%^BOLD%^%^RED%^You get burned by the flames!");
+               tell_object(foes[k], "%^RESET%^%^CRST%^%^C124%^You get burned by the %^C091%^fl%^C097%^a%^C091%^me%^C097%^s%^RESET%^%^C124%^!%^CRST%^");
                dmg = spellob->query_base_damage();
                foes[k]->cause_typed_damage(foes[k], foes[k]->return_target_limb(), dmg, "fire");
                if (objectp(foes[k])) {
@@ -118,13 +118,13 @@ void block(object ob, string exitn) {
 
    level = caster->query_guild_level("mage");
    exitname = exitn;
-   set_short("A wall of fire blocking the "+exitname);
-   set_long("%^BOLD%^%^RED%^You see a massive opaque sheet of fire raging wildly upward in shades of %^MAGENTA%^violet %^RED%^and red.  It fully blocks the "+exitname+".");
+   set_short("%^RESET%^%^CRST%^%^C124%^A wall of %^C091%^f%^C097%^i%^C091%^re %^RESET%^%^C124%^blocking the %^CRST%^"+exitname);
+   set_long("%^RESET%^%^CRST%^%^C124%^You see a massive opaque sheet of fire raging wildly upward in shades of %^C091%^vi%^C097%^o%^C091%^le%^C097%^t %^RESET%^%^C124%^%^and %^C196%^red%^C124%^. It fully blocks the "+exitname+".%^CRST%^");
    whose = caster->query_name();
    wallname = whose+exitname+"firewall";
    if(present(wallname,environment(caster))) {
-      tell_object(caster,"The new wall simply melts into the wall that is already blocking the "+exitname+"\n");
-      tell_room(environment(caster),"The wall blocking the "+exitname+" glows brighter for a second.\n");
+      tell_object(caster,"%^RESET%^%^CRST%^%^C124%^The new wall simply melts into the wall that is already blocking the "+exitname+"\n%^CRST%^");
+      tell_room(environment(caster),"%^RESET%^%^CRST%^%^C124%^The wall blocking the "+exitname+" glows brighter for a second.\n%^CRST%^");
       if(objectp(query_property("spell")))
          query_property("spell")->dest_effect();
       return;
@@ -142,13 +142,13 @@ void remove_wall() {
       if(present(caster,ETO)) {
          notsee = ({caster});
          if(blocking)
-            tell_object(caster,"%^RED%^Your wall of flame"+exitdesc+" dissipates away.");
+            tell_object(caster,"%^RESET%^%^CRST%^%^C124%^Your wall of %^C091%^f%^C097%^l%^C091%^am%^C097%^e%^RESET%^%^C124%^"+exitdesc+" dissipates away.%^CRST%^");
          else {
-            tell_object(caster,"%^RED%^The wall of flame that surrounds you dissipates away.");
+            tell_object(caster,"%^RESET%^%^CRST%^%^C124%^The wall of %^C091%^f%^C097%^l%^C091%^am%^C097%^e%^RESET%^%^C124%^ that surrounds you dissipates away.%^CRST%^");
             caster->move(ETO);
          }
       }
-      tell_room(ETO,"%^RED%^"+caster->query_cap_name()+"'s wall of flame"+exitdesc+" dissipates away.", notsee);
+      tell_room(ETO,"%^RESET%^%^CRST%^%^C124%^"+caster->query_cap_name()+"%^RESET%^%^CRST%^%^C124%^'s wall of %^C091%^f%^C097%^l%^C091%^am%^C097%^e%^RESET%^%^C124%^"+exitdesc+" dissipates away.%^CRST%^", notsee);
    }
    destruct(TO);
    return;
@@ -164,12 +164,12 @@ int damager(string str) {
         if((string)caster->query_name()==(string)TPQN )
             return 0;
     if(TP->query_property("strength") && strsrch(TP->query_property("strength"),"fire") != -1 ) {
-        tell_object(TP,"%^BOLD%^%^RED%^You step into the violet inferno, walking calmly through the flames, unharmed.");
-        tell_room(ETP,"%^BOLD%^%^RED%^"+TPQCN+" walks calmly into the violet inferno blocking the "+exitname+", unaffected by the flames.",TP);
+        tell_object(TP,"%^RESET%^%^CRST%^%^C124%^You step into the %^C091%^vi%^C097%^o%^C091%^le%^C097%^t %^C091%^in%^C097%^f%^C091%^ern%^C097%^o%^RESET%^%^C124%^, walking calmly through the flames, unharmed.%^CRST%^");
+        tell_room(ETP,"%^RESET%^%^CRST%^%^C124%^"+TPQCN+"%^RESET%^%^CRST%^%^C124%^ walks calmly into the %^C091%^vi%^C097%^o%^C091%^le%^C097%^t %^C091%^in%^C097%^f%^C091%^ern%^C097%^o%^RESET%^%^C124%^ blocking the "+exitname+", unaffected by the flames.%^CRST%^",TP);
         return 0;
     }
-    tell_object(TP,"%^BOLD%^%^RED%^You step into the violet inferno, rushing through before the flames engulf you.");
-    tell_room(ETP,"%^BOLD%^%^RED%^"+TPQCN+" rushes into the violet inferno blocking the "+exitname+", as the flames attack "+TP->query_objective()+"!",TP);
+    tell_object(TP,"%^RESET%^%^CRST%^%^C124%^You step into the %^C091%^vi%^C097%^o%^C091%^le%^C097%^t %^C091%^in%^C097%^f%^C091%^ern%^C097%^o%^RESET%^%^C124%^, rushing through before the flames engulf you.%^CRST%^");
+    tell_room(ETP,"%^RESET%^%^CRST%^%^C124%^"+TPQCN+"%^RESET%^%^CRST%^%^C124%^ rushes into the %^C091%^vi%^C097%^o%^C091%^le%^C097%^t %^C091%^in%^C097%^f%^C091%^ern%^C097%^o%^RESET%^%^C124%^ blocking the "+exitname+", as the flames attack "+TP->query_objective()+"!%^CRST%^",TP);
     if(TP->query_property("undead")) dmg = (roll_dice(2,6)+level+level/2)*2;
     else dmg = roll_dice(2,6)+level+level/2;
     TP->cause_typed_damage(TP,TP->return_target_limb(),dmg,"fire" );
