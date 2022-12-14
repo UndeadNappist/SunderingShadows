e3c4b6c4e2a97da1802c4ec0ccfcd5f4c889b311
shadowgate <law@shadowgate.org>
2022-12-14 18:00:03 +0000

    Autocommit

--- cmds/spells/h/_heart_of_metal.c
+++ cmds/spells/h/_heart_of_metal.c
@@ -8,7 +8,7 @@ inherit SPELL;
 #define MYTYPES ({ "silver", "cold iron" })
 
 object mywpn;
-string origtype, weaponarg, typearg, ashort;
+string weaponarg, typearg, ashort;
 int args;
 
 void create()
@@ -49,8 +49,6 @@ int preSpell()
         tell_object(caster, "That is not a valid type of metal!");
         return 0;
     }
-    //origtype = mywpn->query_special_material_type();
-    origtype = mywpn->query_damage_type();
     return 1;
 }
 
@@ -61,7 +59,6 @@ void spell_effect(int prof)
     case "silver":
         ashort = " %^BOLD%^%^WHITE%^{{s%^RESET%^%^WHITE%^i%^BOLD%^%^WHITE%^lv%^RESET%^%^WHITE%^e%^BOLD%^%^WHITE%^ry%^BOLD%^%^WHITE%^}}%^RESET%^";
         mywpn->set_special_material_type("silver");
-        //mywpn->set_damage_type("silver");
         mywpn->remove_property("added short string");
         mywpn->set_property("added short string", ({ ashort }));
         mywpn->set_property("added short", ({ ashort }));
@@ -82,7 +79,6 @@ void spell_effect(int prof)
     case "cold iron":
         ashort = " %^RESET%^%^RED%^{{cold iron}}%^RESET%^";
         mywpn->set_special_material_type("cold iron");
-        //mywpn->set_damage_type("cold iron");
         mywpn->remove_property("added short string");
         mywpn->set_property("added short string", ({ ashort }));
         mywpn->set_property("added short", ({ ashort }));
@@ -111,8 +107,7 @@ void dest_effect()
         mywpn->remove_property_value("added short", ({ ashort }));
         mywpn->remove_property("added short string", ({ ashort }));
         mywpn->remove_property("added short string");
-        //mywpn->set_special_material_type(origtype);
-        mywpn->set_damage_type(origtype);
+        mywpn->set_special_material_type("none");
     }
     if (objectp(TO)) {
         TO->remove();
