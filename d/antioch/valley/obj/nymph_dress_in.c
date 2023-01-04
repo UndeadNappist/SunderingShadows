5b0b66606e97adcfb75665e556f3ae0429799b9d
shadowgate <law@shadowgate.org>
2023-01-04 16:30:02 +0000

    Autocommit

--- d/antioch/valley/obj/nymph_dress_in.c
+++ d/antioch/valley/obj/nymph_dress_in.c
@@ -1,10 +1,10 @@
 //Added a short, flushed out desc and gave lore - also updated the emotes to make it easier to rp, because i know crystal wanted to rewrite them anyway - Cythera 5/05\\
 // updated cha bonus from the new system. Nienne, 09/07.
+// Removed body typing restrictions, aggressive language regarding genders, and leading reactions from observers. Chernobog (1/4/23)
 #include <std.h>
 #include <daemons.h>
 #include "../valley.h"
 inherit "/std/armour";
-int FLAG;
 
 object *women;
 object *men;
@@ -67,68 +67,45 @@ void create()
 	set_wear((:TO,"wearme":));
 	set_remove((:TO,"removeme":));
 	set_heart_beat(1);
-FLAG=0;
 }
 
-int wearme()
-{
+int wearme(){
 	if(ETO->query_level() < 10) {
 		write("You are not ready for a dress this lovely.");
 		return 0;
 	}
-  if (member_array((string)ETO->query_body_type(), ({ "plump", "well-built", "hardy" ,"stocky", "portly", "stout", "brawny", "massive", "rotund","heavy" }) ) != -1 ){
-      write("Your body type simply doesn't fit into this dainty dress.");
-      return 0;
-   }
-	if((string)ETO->query_gender() == "male") {
-		tell_object(ETO,"%^BOLD%^%^RED%^You slip into the black"+
-		" velvet dress.");
-		tell_room(environment(ETO),"%^BOLD%^%^YELLOW%^"+ETO->query_cap_name()+""+
-		" akwardly works his way into the black velvet dress, getting"+
-		" in touch with his feminine side.",ETO);
-		set_item_bonus("charisma",-1);
-		return 1;
-	}
-	if((string)ETO->query_gender() == "female") {
-		tell_object(ETO,"%^BOLD%^%^RED%^You slip on the black"+
-		" velvet dress, it feels wonderful to the touch and seems"+
-		" to magically fit itself to your body, showing off"+
-		" every curve.");
-		tell_room(environment(ETO),"%^BOLD%^%^RED%^"+ETO->query_cap_name()+""+
-		" slips slowly into her black velvet dress, relishing"+
-		" the feel of it against her skin. The dress seems to be"+
-		" made just for her, showing off every curve of her body.",ETO);
-		set_item_bonus("charisma",1);
-		return 1;
-	}
-	else {
-		notify_fail("There seems to be something wrong with you,"+
-		" go call for help before it's too late!");
-		return 0;
-	}
+    switch((string)ETO->query_gender()){
+        case "male":
+            tell_object(ETO,"%^BOLD%^%^RED%^You slip into the black velvet dress.");
+            tell_room(environment(ETO),"%^BOLD%^%^YELLOW%^"+ETO->query_cap_name()+" works his way into the black velvet dress, getting in touch with his feminine side.",ETO);
+            break;
+        case "female":
+            tell_object(ETO,"%^BOLD%^%^RED%^You slip on the black velvet dress, it feels wonderful to the touch and seems to magically fit itself to your body, showing off every curve.");
+            tell_room(environment(ETO),"%^BOLD%^%^RED%^"+ETO->query_cap_name()+" slips slowly into her black velvet dress, relishing the feel of it against her skin. The dress seems to be made just for her, showing off every curve of her body.",ETO);
+            set_item_bonus("charisma", 1);
+            break;
+        default:
+            tell_object(ETO,"%^BOLD%^%^RED%^You slip into the black velvet dress.");
+            tell_room(environment(ETO),"%^BOLD%^%^YELLOW%^"+ETO->query_cap_name()+" works their way into the black velvet dress, getting in touch with their feminine side.",ETO);
+    }
+    return 1;
 }
 
-int removeme()
-{
-	if((string)ETO->query_gender() == "male") {
-		tell_object(ETO,"%^MAGENTA%^You slip out of the black"+
-		" velvet dress.");
-		tell_room(environment(ETO),"%^ORANGE%^"+ETO->query_cap_name()+""+
-		" slowly takes off his black velvet dress. You have to"+
-		" wonder about the type of man who actually wears such"+
-		" a thing...",ETO);
-		return 1;
+int removeme(){
+    switch((string)ETO->query_gender()){
+        case "male":
+            tell_object(ETO,"%^MAGENTA%^You slip out of the black velvet dress.");
+            tell_room(environment(ETO),"%^ORANGE%^"+ETO->query_cap_name()+" slowly takes off his black velvet dress.",ETO);
+            break;
+        case "female":
+            tell_object(ETO,"%^MAGENTA%^You slowly remove the black velvet dress, already you miss the feel of the crushed velvet against your skin.");
+            tell_room(environment(ETO),"%^MAGENTA%^"+ETO->query_cap_name()+" very slowly slides the black velvet dress off of her shoulders, letting it drop to the floor with a wistful expression.",ETO);
+            break;
+        default:
+            tell_object(ETO,"%^MAGENTA%^You slip out of the black velvet dress.");
+            tell_room(environment(ETO),"%^ORANGE%^"+ETO->query_cap_name()+" slowly takes off their black velvet dress.",ETO);
 	}
-	else {
-		tell_object(ETO,"%^MAGENTA%^You slowly remove the"+
-		" black velvet dress, already you miss the feel of the"+
-		" crushed velvet against your skin.");
-		tell_room(environment(ETO),"%^MAGENTA%^"+ETO->query_cap_name()+""+
-		" very slowly slides the black velvet dress off of her"+
-		" shoulders, letting it drop to the floor with a"+
-		" wistful expression.",ETO);
-		return 1;
-	}
+    return 1;
 }
 
 void gender_arrays(object room)
@@ -203,17 +180,6 @@ void heart_beat()
 		}
 		return;
 	}
-	
-	if((string)ETO->query_gender() == "male") {
-		dumb_male();
-		return;
-	}
-
-	/*else {
-		tell_room(environment(ETO),""+ETO->query_cap_name()+""+
-		" is a freak.",ETO);
-		return;
-	}*/
 }
 
 object *query_men()
@@ -417,8 +383,7 @@ void high_cha()
 	}
 }
 
-void goddess()
-{
+void goddess(){
 	int yy;
 
 	yy = random(14)+1;
@@ -474,9 +439,9 @@ void goddess()
 	}
 	if(yy == 10) {
 		tell_object(targman,"%^BOLD%^%^BLACK%^The hem of "+ETO->QCN+"'s dress rides"+
-			" up a bit, giving you a glimps of her slender legs .");
+			" up a bit, giving you a glimpse of her slender legs .");
 		message("info","%^BOLD%^%^BLACK%^The hem of "+ETO->QCN+"'s dress rides up a bit"+
-			", giving you a glimps of her slender legs.",othermen);
+			", giving you a glimpse of her slender legs.",othermen);
 		return;
 	}
 	if(yy == 11) {
@@ -504,112 +469,6 @@ void goddess()
 			"%^BOLD%^l%^RESET%^v%^WHITE%^e%^RESET%^r%^BOLD%^%^BLACK%^"+
 			" sparkles on "+ETO->QCN+"'s gown twinkle,"+
 			" drawing attention to how the dress molds to her body.",men);
-		return;
-	}
-}
-
-void dumb_male()
-{
-	int yy;
-
-	yy = random(14)+1;
-	if(yy == 1) {
-		tell_room(environment(ETO),""+ETO->query_cap_name()+""+
-		" grins broadly and admires his dress.",ETO);
-		return;
-	}
-	if(yy == 2) {
-		tell_room(environment(ETO),""+ETO->query_cap_name()+""+
-		" dances around merrily in his pretty black velvet dress.",ETO);
-		return;
-	}
-	if(yy == 3) {
-		tell_room(environment(ETO),""+ETO->query_cap_name()+""+
-		" laughs like a woman.",ETO);
-		return;
-	}
-	if(yy == 4) {
-		tell_room(environment(ETO),""+ETO->query_cap_name()+""+
-		" takes out a brush and begins to meticulously brush"+
-		" his hair.",ETO);
-		return;
-	}
-	if(yy == 5) {
-		tell_room(environment(ETO),""+ETO->query_cap_name()+""+
-		" starts applying makeup, getting ready for a night"+
-		" on the town.",ETO);
-		return;
-	}
-	if(yy == 6) {
-		tell_room(environment(ETO),""+ETO->query_cap_name()+""+
-		" giggles like a little girl.",ETO);
-		return;
-	}
-	if(yy == 7) {
-		tell_room(environment(ETO),""+ETO->query_cap_name()+""+
-		" starts putting on some tights and black high heels to"+
-		" match his dress.",ETO);
-		return;
-	}
-	if(yy == 8) {
-		tell_room(environment(ETO),""+ETO->query_cap_name()+""+
-		" has his dress tucked in to his tights at the back.",ETO);
-		return;
-	}
-	if(yy == 9) {
-		if(sizeof(men) > 0) {
-			message("info",""+ETO->query_cap_name()+""+
-			" winks lewdly and proceeds to hit on you.",men);
-			message("info",""+ETO->query_cap_name()+" hits on"+
-			" "+targman->query_cap_name()+".",women);
-			return;
-		}
-		return;
-	}
-	if(yy == 10) {
-		tell_room(environment(ETO),""+ETO->query_cap_name()+""+
-		" sucks on his thumb.",ETO);
-		return;
-	}
-	if(yy == 11) {
-		if(sizeof(men) > 0) {
-			tell_object(targman,""+ETO->query_cap_name()+" sits"+
-			" down in your lap.");
-			message("info",""+ETO->query_cap_name()+" sits in"+
-			" "+targman->query_cap_name()+"'s lap.",othermen);
-			message("info",""+ETO->query_cap_name()+" sits in"+
-			" "+targman->query_cap_name()+"'s lap.",women);
-			return;
-		}
-		return;
-	}
-	if(yy == 12) {
-		if(sizeof(men) > 0) {
-			tell_object(targman,""+ETO->query_cap_name()+" pinches"+
-			" your butt!");
-			message("info",""+ETO->query_cap_name()+" pinches"+
-			" "+targman->query_cap_name()+"'s butt!",othermen);
-			message("info",""+ETO->query_cap_name()+" pinches"+
-			" "+targman->query_cap_name()+"'s butt!",women);
-			return;
-		}
-		return;
-	}
-	if(yy == 13) {
-		if(sizeof(men) > 0) {
-			tell_object(targman,"%^BOLD%^%^CYAN%^"+ETO->query_cap_name()+""+
-			" whispers to you: %^RESET%^Do I look hot or what?");
-			message("info","%^CYAN%^"+ETO->query_cap_name()+""+
-			" whispers something to "+targman->query_cap_name()+".",othermen);
-			message("info","%^CYAN%^"+ETO->query_cap_name()+""+
-			" whispers something to "+targman->query_cap_name()+".",women);
-			return;
-		}
-		return;
-	}
-	if(yy == 14) {
-		tell_room(environment(ETO),""+ETO->query_cap_name()+""+
-		" begins to do a horrible strip tease just for you.",ETO);
 		return;
 	}
 }
