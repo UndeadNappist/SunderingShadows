a49219f964dbf65520ff24dc73b994e0975e4523
shadowgate <law@shadowgate.org>
2023-01-06 03:30:03 +0000

    Autocommit

---
+++ d/magic/symbols/nilith_symbol.c
@@ -0,0 +1,18 @@
+#include <std.h>
+inherit "/d/magic/symbols/holy_symbol.c";
+
+void create(){
+    ::create();
+    set_id(({"holy symbol", "nilith_holy_symbol", "symbol", "medallion"}));
+    set_name("holy symbol");
+    set_short("holy symbol");
+    set_long("This is a holy symbol.");
+}
+    
+void init(){
+    ::init();
+    set_name("gold medallion");
+    set_short("%^RESET%^%^C244%^A %^RESET%^%^C136%^g%^C220%^o%^C226%^l%^RESET%^%^C136%^d m%^RESET%^%^C178%^e%^C220%^d%^C226%^a%^C227%^l%^C226%^l%^C220%^i%^C178%^o%^RESET%^%^C136%^n %^RESET%^%^C244%^depicting a %^RESET%^%^C230%^skeletal hand %^RESET%^%^C244%^grasping a %^RESET%^%^C143%^w%^C144%^i%^C230%^l%^C015%^t%^C144%^e%^RESET%^%^C143%^d %^RESET%^%^C015%^c%^C230%^a%^C229%^l%^C230%^l%^RESET%^%^C015%^a l%^RESET%^%^C230%^i%^C228%^l%^RESET%^%^C230%^y%^CRST%^");
+    set_long("%^RESET%^%^CRST%^%^C136%^P%^C178%^o%^C220%^u%^C226%^n%^C220%^d%^C178%^e%^RESET%^%^C136%^d %^RESET%^%^C244%^and %^RESET%^%^C220%^shaped%^RESET%^%^C244%^ into a disc, this %^RESET%^%^C136%^m%^RESET%^%^C178%^e%^C220%^d%^C226%^a%^C227%^l%^C226%^l%^C220%^i%^C178%^o%^RESET%^%^C136%^n %^RESET%^%^C244%^appears to be made from solid %^RESET%^%^C226%^gold%^RESET%^%^C244%^. Emblazoned across its center is a bas relief of a %^RESET%^%^C136%^tower shield%^RESET%^%^C244%^, an %^RESET%^%^C230%^ivory inlay %^RESET%^%^C244%^superimposed over that, depicting a %^RESET%^%^C230%^skeletal hand %^RESET%^%^C244%^holding a %^RESET%^%^C143%^w%^C144%^i%^C230%^l%^C015%^t%^C144%^e%^RESET%^%^C143%^d %^RESET%^%^C015%^c%^C230%^a%^C229%^l%^C230%^l%^RESET%^%^C015%^a l%^RESET%^%^C230%^i%^C228%^l%^RESET%^%^C230%^y%^RESET%^%^C244%^. A %^RESET%^%^C241%^small hole %^RESET%^%^C244%^has been drilled into the top of the symbol, a delicate %^RESET%^%^C136%^g%^C178%^o%^C220%^l%^C226%^d%^C178%^e%^RESET%^%^C136%^n c%^RESET%^%^C178%^h%^C220%^a%^C178%^i%^RESET%^%^C136%^n %^RESET%^%^C244%^attached.%^CRST%^");
+    
+}
