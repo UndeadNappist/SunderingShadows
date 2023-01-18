85d23a78ddef820834c50360028ea5ff526fe6bf
shadowgate <law@shadowgate.org>
2023-01-18 01:00:01 +0000

    Autocommit

--- cmds/mortal/_suicide.c
+++ cmds/mortal/_suicide.c
@@ -151,6 +151,7 @@ nomask private int really_suicide(string typed_password)
     }
     message("notify", "%^YELLOW%^<< " + TP->query_name() + " has committed suicide! >>\n", filter_array(users(), "wizuserp", TO));
     "/cmds/avatar/_note.c"->cmd_note("ckpt " + TP->query_name() + " committed suicide!\n");
+    TP->reset_relationships();
     TP->remove();
     return 1;
 }
