dad1f7ac8513c63698d19dca82e22ef6767bd981
shadowgate <law@shadowgate.org>
2023-01-25 13:00:02 +0000

    Autocommit

--- cmds/spells/f/_frightful_aspect.c
+++ cmds/spells/f/_frightful_aspect.c
@@ -18,7 +18,7 @@ void create()
     set_syntax("cast CLASS frightful aspect");
     set_spell_sphere("alteration");
     set_bonus_type(({ "size", "body", "natural armor" }));
-    set_description("You become a larger, awful version of yourself. You grow in size, and take on features that horrify your enemies. You gain the following abilities: a +6 size bonus to Strength, a +4 size bonus to Constitution, a +6 natural armor bonus, 5 damage resistance and spell damage resistance equal to 10 + half your caster level. You also emit an aura that emanates 30 feet from you. Enemy creatures within the aura must make a will save or are shaken.\n\n%^BOLD%^%^RED%^See also:%^RESET%^ status effects");
+    set_description("You become a larger, awful version of yourself. You grow in size, and take on features that horrify your enemies. You gain the following abilities: a +6 size bonus to Strength, a +4 size bonus to Constitution, a +6 natural armor bonus, +5 damage resistance and spell damage resistance equal to 10 + half your caster level. You also emit an aura that emanates 30 feet from you. Enemy creatures within the aura must make a will save or are shaken.\n\n%^BOLD%^%^RED%^See also:%^RESET%^ status effects");
     set_save("will");
     set_verbal_comp();
     set_somatic_comp();
