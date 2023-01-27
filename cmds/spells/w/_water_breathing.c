eb71e93256135ea1a5a4c1428e1b4a644d180ad6
shadowgate <law@shadowgate.org>
2023-01-27 13:30:02 +0000

    Autocommit

--- cmds/spells/w/_water_breathing.c
+++ cmds/spells/w/_water_breathing.c
@@ -13,7 +13,7 @@ void create(){
     set_spell_level(([ "mage" : 3, "cleric" : 3, "oracle" : 3, "druid" : 3, "psywarrior" : 1, "psion" : 1, "magus" : 3 ]));
     set_mystery("waves");
     set_spell_sphere("alteration");
-    set_syntax("cast CLASS water breathing on TARGET");
+    set_syntax("cast CLASS water breathing [on TARGET]");
     set_description("This spell allows the target to breathe water for a limited period of time.");
     set_verbal_comp();
     set_somatic_comp();
@@ -22,44 +22,52 @@ void create(){
 }
 
 int preSpell(){
+    if(!target || !objectp(target)) target = caster;
+    
     if(present("underwtr_breath_ob", target)){
-      tell_object(caster, target->QCN+" can already breathe water.");
-      return 0;
+        tell_object(caster, "%^RESET%^%^CRST%^%^C059%^"+target->query_cap_name()+"%^RESET%^%^CRST%^%^C059%^ can already breathe water.%^CRST%^");
+        return 0;
     }
     return 1;
 }
 
-void spell_effect(int prof) {
-    if (environment(target)!=place) {
-      tell_object(caster,"%^BOLD%^Your target has left the area!");
-      TO->remove();
-      return;
+void spell_effect(int prof){
+    if (environment(target)!=place){
+        tell_object(caster,"%^RESET%^%^CRST%^%^C059%^Your target has left the area!%^CRST%^");
+        this_object()->remove();
+        return;
     }
 
     spell_successful();
-    tell_room(environment(caster), "%^BOLD%^%^CYAN%^"+caster->QCN+" touches "+target->QCN+"'s nose and mouth lightly.", ({caster, target}) );
-    tell_object(caster, "%^BOLD%^%^CYAN%^You give "+target->QCN+" the ability to breathe water.");
-    tell_object(target, "%^BOLD%^%^CYAN%^"+caster->QCN+" gives you the power to breathe water.");
+    tell_room(environment(caster), "%^RESET%^%^CRST%^%^C051%^"+caster->query_cap_name()+"%^RESET%^%^CRST%^%^C051%^ touches "+target->possessive()+" nose and mouth lightly.%^CRST%^", ({caster, target}) );
+    if(caster == target){
+        tell_object(caster, "%^RESET%^%^CRST%^%^C051%^You touch your nose and mouth lightly, granting yourself the ability to breathe water.%^CRST%^");
+    }
+    else{
+        tell_object(caster, "%^RESET%^%^CRST%^%^C051%^You touch "+target->query_cap_name()+"%^RESET%^%^CRST%^%^C051%^'s nose and mouth lightly, granting "+target->query_objective()+" the ability to breathe water.%^CRST%^");
+        tell_object(target, "%^RESET%^%^CRST%^%^C051%^"+caster->query_cap_name()+"%^RESET%^%^CRST%^%^C051%^ touches your nose and mouth lightly, granting you the power to breathe water.%^CRST%^");
+    }
 
     breathob = new(WTROB);
     spell_duration = (clevel + roll_dice(1, 20)) * ROUND_LENGTH;
     set_end_time();
     breathob->set_maxtime(spell_duration);
-    breathob->set_property("spell", TO);
-    breathob->set_property("spelled", ({TO}));
+    breathob->set_property("spell", this_object());
+    breathob->set_property("spelled", ({this_object()}));
     addSpellToCaster();
     breathob->move(target);
 }
 
 void dest_effect(){
-    tell_object(target, "%^BOLD%^%^CYAN%^You feel the power to breathe water leave you.");
+    tell_object(target, "%^RESET%^%^CRST%^%^C059%^You feel the power to breathe %^C033%^w%^C039%^a%^C045%^t%^C033%^e%^C039%^r %^RESET%^%^C059%^leave you.%^CRST%^");
     if(objectp(breathob))
       breathob->remove();
     ::dest_effect();
-    if(objectp(TO)) TO->remove();
+    if(objectp(this_object())) this_object()->remove();
 
 }
 
 void reverse_spell(){
     spell_effect(-100);
 }
+
