f8d82827aa09a48f469a9149613bad7b8b2e27fb
shadowgate <law@shadowgate.org>
2023-02-06 00:00:02 +0000

    Autocommit

--- cmds/spells/s/_speak_with_dead.c
+++ cmds/spells/s/_speak_with_dead.c
@@ -8,7 +8,7 @@ void create()
 {
     ::create();
     set_spell_name("speak with dead");
-    set_spell_level(([ "cleric" : 3, "inquisitor" : 3, "bard" ]));
+    set_spell_level(([ "cleric" : 3, "inquisitor" : 3, "bard" : 3 ]));
     set_spell_sphere("divination");
     set_syntax("cast CLASS speak with dead to TARGET");
     set_description("This will allow the caster to attempt to establish "+
