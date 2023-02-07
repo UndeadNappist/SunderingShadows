595062f5ee1054b0e3dc985c3a96f6288f10f51e
shadowgate <law@shadowgate.org>
2023-02-07 20:30:02 +0000

    Autocommit

--- cmds/feats/e/_eldritch_melding.c
+++ cmds/feats/e/_eldritch_melding.c
@@ -9,7 +9,7 @@ void create()
    feat_category("PactBond");
    feat_name("eldritch melding");
    feat_prereq("Warlock L14");
-   feat_desc("As the tether to your patron grows, certain secrets are revealed. Shortcuts, if you will. Why study an object for hours to determine its intricacies when greater powers already possess such knowledge? What is a little more soul compared to vast applications of the arcane with a mere snap of the fingers? With this feat, the warlock gains +5 when rolling to use off-class spells on wands and other enchanted items. (this does not apply to scrolls)");
+   feat_desc("As the tether to your patron grows, certain secrets are revealed. Shortcuts, if you will. Why study an object for hours to determine its intricacies when greater powers already possess such knowledge? What is a little more soul compared to vast applications of the arcane with a mere snap of the fingers? With this feat, the warlock gains +10 when rolling to use off-class spells on wands and other enchanted items. (this does not apply to scrolls)");
    permanent(1);
    allow_blind(1);
    set_required_for(({}));
