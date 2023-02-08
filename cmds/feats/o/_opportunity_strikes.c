1a9cf34226893aabd71934f890bf64ae43ae21af
shadowgate <law@shadowgate.org>
2023-02-08 17:30:02 +0000

    Autocommit

--- cmds/feats/o/_opportunity_strikes.c
+++ cmds/feats/o/_opportunity_strikes.c
@@ -12,7 +12,7 @@ void create()
     feat_name("opportunity strikes");
     feat_desc("This will allow character to position themself better in combat, and to strike with greater strength. It will increase damage done if they wield one one-handed weapon.
 
-%^BOLD%^N.B.%^RESET%^ This feat tree won't work with shield worn.");
+%^BOLD%^N.B.%^RESET%^ This feat won't work with shield worn.");
     permanent(1);
 }
 
