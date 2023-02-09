8dc2b0d99d54c310fd4fe94faef763fc7a13917c
shadowgate <law@shadowgate.org>
2023-02-09 14:00:02 +0000

    Autocommit

--- cmds/spells/i/_inspiring_recovery.c
+++ cmds/spells/i/_inspiring_recovery.c
@@ -4,7 +4,7 @@
 
 inherit SPELL;
 
-create() {
+create(){
     ::create();
     set_spell_name("inspiring recovery");
     set_spell_level(([ "psion" : 5 ]));
@@ -12,10 +12,7 @@ create() {
     set_discipline("egoist");
     set_syntax("cast CLASS inspiring recovery on TARGET");
     set_damage_desc("healing or revive recently deceased");
-    set_description("You channel psychic energy into the target, helping their body repair their wounds or resuscitate them if they're deceased. Does not work on undead creatures or constructs.
-
-This spell accepts the recognized player's name or their corpse as the TARGET if they are deceased.
-");
+    set_description("You channel psychic energy into the target, helping their body repair their wounds or resuscitate them if they're deceased. Does not work on undead creatures or constructs.\n\nThis spell accepts the recognized player's name or their corpse as the TARGET if they are deceased.");
     set_verbal_comp();
     set_somatic_comp();
     set_arg_needed();
@@ -23,91 +20,66 @@ This spell accepts the recognized player's name or their corpse as the TARGET if
 	set_helpful_spell(1);
 }
 
-string what_alignment(int align)
-{
-    string alignment;
-    switch(align)
-    {
-    case 1: alignment = "Lawful Good"; break;
-    case 2: alignment = "Lawful Neutral"; break;
-    case 3: alignment = "Lawful Evil"; break;
-    case 4: alignment = "Neutral Good"; break;
-    case 5: alignment = "True Neutral"; break;
-    case 6: alignment = "Neutral Evil"; break;
-    case 7: alignment = "Chaotic Good"; break;
-    case 8: alignment = "Chaotic Neutral"; break;
-    case 9: alignment = "Chaotic Evil"; break;
-    }
-    return alignment;
-}
-
-spell_effect(int prof) {
+spell_effect(int prof){
     string what, theirname, dam_type;
     object corpse, targ;
 
     ::spell_effect();
 
-    if(!objectp(target))
-    {
+    if(!objectp(target)){
         theirname = caster->realName(lower_case(arg));
         target = find_player(theirname);
     }
 
-    if(!objectp(target))
-    {
-        tell_object(caster,"The target is needed.");
+    if(!objectp(target)){
+        tell_object(caster, "%^RESET%^%^CRST%^%^C059%^You need a target.%^CRST%^");
         dest_effect();
     }
 
-    if(target->is_corpse()||target->query_ghost())
-    {
-        if(target->is_corpse())
-            corpse = target;
-        else
-            corpse = present("corpse of $&$"+target->query_true_name()+"$&$",place);
+    if(target->is_corpse()||target->query_ghost()){
+        if(target->is_corpse()) corpse = target;
+        else corpse = present("corpse of $&$"+target->query_true_name()+"$&$",place);
 
-        if (!interactive(target)) {
-            tell_object(caster,"You can only revive interactive players.");
+        if(!interactive(target)){
+            tell_object(caster, "%^RESET%^%^CRST%^%^C059%^You can only revive interactive players.%^CRST%^");
             dest_effect();
             return;
         }
-        if (interactive(caster) ) {
-            set_cast_string(caster->QCN+" calls upon the magic!\n");
-            tell_object(caster, "%^ORANGE%^You attempt to resuscitate "+capitalize(arg)+".");
-            tell_room(environment(CASTER), "%^ORANGE%^"+caster->QCN+" tries to resuscitate "+capitalize(arg)+".", ({ caster, target}));
-        } else {
-            set_cast_string(caster->QCN+" channels psychic energy into "+capitalize(arg)+", attempting to resuscitate them!\n");
+        if(interactive(caster)){
+            set_cast_string("%^RESET%^%^CRST%^%^C101%^"+caster->query_cap_name()+"%^RESET%^%^CRST%^%^C101%^ focuses intently...%^CRST%^\n");
+            tell_object(caster, "%^RESET%^%^CRST%^%^C101%^You attempt to resuscitate "+capitalize(arg)+"%^RESET%^%^CRST%^%^C101%^!%^CRST%^");
+            tell_room(environment(CASTER), "%^RESET%^%^CRST%^%^C101%^"+caster->query_cap_name()+"%^RESET%^%^CRST%^%^C101%^ tries to resuscitate "+capitalize(arg)+"%^RESET%^%^CRST%^%^C101%^!%^CRST%^", ({ caster, target}));
         }
-        if (!target->query_ghost()) {
-            tell_object(caster,capitalize(arg)+" does not need to be revived.");
+        else{
+            set_cast_string("%^RESET%^%^CRST%^%^C101%^" + caster->query_cap_name()+"%^RESET%^%^CRST%^%^C101%^ channels %^C206%^p%^C200%^s%^C218%^y%^C212%^c%^C206%^h%^C200%^i%^C218%^c %^C212%^e%^C206%^n%^C200%^e%^C218%^r%^C212%^g%^C206%^y %^C144%^into "+capitalize(arg)+"%^RESET%^%^CRST%^%^C101%^, attempting to resuscitate them!%^CRST%^\n");
+        }
+        if(!target->query_ghost()){
+            tell_object(caster, "%^RESET%^%^CRST%^%^C059%^" + capitalize(arg) + "%^RESET%^%^CRST%^%^C059%^ does not need to be revived.%^CRST%^");
             dest_effect();
             return;
         }
-        if (!objectp(corpse)) {
-            tell_object(caster,capitalize(arg)+"'s corpse is not here.");
-            tell_object(target,"You think you feel a faint pull on your soul, but it is gone as soon as it begins.");
+        if(!objectp(corpse)){
+            tell_object(caster, "%^RESET%^%^CRST%^%^C059%^" + capitalize(arg) + "%^RESET%^%^CRST%^%^C059%^'s corpse is not here.%^CRST%^");
+            tell_object(target, "%^RESET%^%^CRST%^%^C059%^You think you feel a faint pull on your soul, but it is gone as soon as it begins.%^CRST%^");
             dest_effect();
             return;
         }
-        tell_object(caster,"%^ORANGE%^You can feel psychic energy flow out to "+target->QCN+"'s body, and know that "+target->QP+" life is in "+target->QP+" own hands now.");
-        tell_room(environment(caster),"The power of "+caster->QCN+"'s psychic energy flows through the area as "+caster->QS+" tries to bring "+target->QCN+" back to life!",caster);
+        tell_object(caster, "%^RESET%^%^CRST%^%^C144%^You can feel %^C206%^p%^C200%^s%^C218%^y%^C212%^c%^C206%^h%^C200%^i%^C218%^c %^C212%^e%^C206%^n%^C200%^e%^C218%^r%^C212%^g%^C206%^y %^C144%^flow out to "+target->query_cap_name()+"%^RESET%^%^CRST%^%^C144%^'s body, and know that "+target->query_possessive()+" life is in "+target->query_possessive()+" own hands now.%^CRST%^");
+        tell_room(environment(caster), "%^RESET%^%^CRST%^%^C144%^The power of "+caster->query_cap_name()+"%^RESET%^%^CRST%^%^C144%^'s %^C206%^p%^C200%^s%^C218%^y%^C212%^c%^C206%^h%^C200%^i%^C218%^c %^C212%^e%^C206%^n%^C200%^e%^C218%^r%^C212%^g%^C206%^y %^C144%^flows through the area as "+caster->query_subjective()+" tries to bring "+target->query_cap_name()+"%^RESET%^%^CRST%^%^C144%^ back to life!%^CRST%^", caster);
         target->set("RaisingPriestGod",caster->query_property("hidden deity")||caster->query_diety());
         target->set("RaisingPriestAlignment",caster->query_alignment());
         target->set("RaisingRoom",base_name(environment(caster)));
         target->set("RaisingExpLoss",(-10+(random(2)+random(3)+1)));
         target->set("RaisingType","raise dead");
-        tell_object(target,"%^RESET%^%^WHITE%^%^BOLD%^You can feel a pull on your soul. You sense that psychic energy is trying to return you to consciousness!\nType %^ORANGE%^<accept>%^WHITE%^ to return to life, or %^ORANGE%^<cancel>%^WHITE%^ to die.%^RESET%^");
+        tell_object(target, "%^RESET%^%^CRST%^%^C144%^You can feel a pull on your soul. You sense a powerful %^C206%^p%^C200%^s%^C218%^y%^C212%^c%^C206%^h%^C200%^i%^C218%^c %^C212%^e%^C206%^n%^C200%^e%^C218%^r%^C212%^g%^C206%^y %^C144%^is trying to return you to consciousness!\n%^RESET%^%^CRST%^%^C059%^Type %^C255%^<accept>%^C059%^ to return to life, or %^C255%^<cancel>%^C059%^ to die.%^CRST%^");
         corpse->remove();
     }
-    else
-    {
-        if(target->is_undead() || USER_D->is_valid_enemy(target->query_race(), "constructs"))
-        {
-            tell_room(place,"%^BLUE%^"+caster->QCN+" places "+caster->QP+" hand on "+target->QCN+" and channels psychic energy into them, but nothing happens!", caster);
+    else{
+        if(target->is_undead() || USER_D->is_valid_enemy(target->query_race(), "constructs")){
+            tell_room(place, "%^RESET%^%^CRST%^%^C144%^"+caster->query_cap_name()+"%^RESET%^%^CRST%^%^C144%^ places "+caster->query_possessive()+" hand on "+target->query_cap_name()+"%^RESET%^%^CRST%^%^C144%^ and channels %^C206%^p%^C200%^s%^C218%^y%^C212%^c%^C206%^h%^C200%^i%^C218%^c %^C212%^e%^C206%^n%^C200%^e%^C218%^r%^C212%^g%^C206%^y %^C144%^into them, but %^C059%^nothing happens%^C144%^!%^CRST%^", caster);
         }
-        else
-        {
-            tell_room(place,"%^ORANGE%^"+caster->QCN+" places "+caster->QP+" hand on "+target->QCN+" wills their body to heal!");
+        else{
+            tell_room(place,"%^RESET%^%^CRST%^%^C144%^"+caster->query_cap_name()+"%^RESET%^%^CRST%^%^C144%^ places "+caster->query_possessive()+" hand on "+target->query_cap_name()+"%^RESET%^%^CRST%^%^C144%^ and wills their body to %^C255%^heal%^C144%^!%^CRST%^");
             damage_targ(target, "torso", -sdamage, "untyped");
         }
     }
@@ -116,5 +88,5 @@ spell_effect(int prof) {
 
 void dest_effect(){
     ::dest_effect();
-    if(objectp(TO)) TO->remove();
+    if(objectp(this_object())) this_object()->remove();
 }
