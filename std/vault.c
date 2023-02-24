76cacf5518b864be5d264a0f22b5745a9e6c6dee
shadowgate <law@shadowgate.org>
2023-02-24 03:00:02 +0000

    Autocommit

--- std/vault.c
+++ std/vault.c
@@ -1000,7 +1000,7 @@ string query_long_exits()
         break;
 
     case 1:
-        ndstr = "The only obvious exit is: " + NonDoorExits[0] + ".\n";
+        ndstr = "The only obvious exit is: " + NonDoorExits[0] + ".";
         break;
 
     default:
