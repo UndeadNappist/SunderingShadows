f2fc2e9abc2c372da0928ebd5e69f5b955aeee1d
shadowgate <law@shadowgate.org>
2023-02-26 21:00:02 +0000

    Autocommit

--- d/dagger/Torm/obj/tambourine.c
+++ d/dagger/Torm/obj/tambourine.c
@@ -8,7 +8,7 @@ void create()
    set_short("A tambourine");
    set_id(({"tambourine"}));
    set_long("This is a rather uncommon instrument, but it is quite"+
-   " useful, especially preferred by gypsies and is great to use"+
+   " useful, especially preferred by some nomadic tribes and is great to use"+
    " while dancing. It consists of a stretch of very thin dried hide"+
    " that has been pulled taut across a round circle of wood that has"+
    " been set with tiny pairs of steel symbols that clink together"+
