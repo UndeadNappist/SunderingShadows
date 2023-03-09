21fa1b4f5480883adc8e978b0e0650c7688876bf
shadowgate <law@shadowgate.org>
2023-03-09 11:30:02 +0000

    Autocommit

--- d/magic/obj/holder.c
+++ d/magic/obj/holder.c
@@ -185,7 +185,7 @@ int cmd(string str)
 int dismiss(string str){
 
 	if(!str || !mon->id(str))
-		return 0;
+        return notifY_fail("Dismiss what?\n");
 
 	if(freed)
 		return notify_fail("You have no power over this being any more.\n");
